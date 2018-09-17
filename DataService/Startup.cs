285c03b2181021b40617f03b6dadf1e3341f3500
Mauricio Rodriguez <mauro1@hotmail.com>
2018-09-17 18:16:31 -0400

    label

--- DataService/Startup.cs
+++ DataService/Startup.cs
@@ -235,6 +235,7 @@ namespace DataService
                 string page = "";
                 if (label.Contains("|"))
                 {
+                    //Dont remove 
                     string[] array = label.Split('|');
                     dom = array[0].Replace("www.", "").Replace("http://", "").Replace("https://", "").Trim();
                     page = array[1];
