c6b747b5c6063494c0013cdd27897a1852a81acd
Josh Greenberg <greenbergjosh@hotmail.com>
2018-10-11 12:09:35 -0400

    test

--- UnsubJob/Program.cs
+++ UnsubJob/Program.cs
@@ -24,7 +24,7 @@ namespace UnsubJob
                                 "kerBVnPFmJ");
 
             await Utility.ProtocolClient.DownloadFileFtp(@"c:\workspace\unsub",
-                "2f4d4b49-4f9b-4ec9-97cb-21b1c3cef676.txt.upl",
+                "Unsub/2f4d4b49-4f9b-4ec9-97cb-21b1c3cef676.txt.upl",
                 "2f4d4b49-4f9b-4ec9-97cb-21b1c3cef676.txt.dwl",
                 "ftpback-bhs6-85.ip-66-70-176.net",
                 "ns557038.ip-66-70-182.net",
