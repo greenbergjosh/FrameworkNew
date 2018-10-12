0f2ab3a9b74f7e8683e891f74e7fe971593ab222
Agustin Young <ayoung@onpointglobal.com>
2018-10-12 18:42:56 -0300

    add mailprocess

---
+++ MailProcess/AsyncController.cs
@@ -0,0 +1,94 @@
+﻿using BusinessLogic;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using BusinessLogic.Model;
+using SharpRaven.Data;
+using SharpRaven;
+using NLog;
+
+namespace mail_app_process
+{
+    internal class AsyncController
+    {
+        public async Task AsyncLoadData(List<Seed> gmailSeeds, List<Seed> outlookSeeds, List<Seed> aolSeeds, List<Seed> yahooSeeds)
+        {
+            try
+            {
+                List<Task> tasksAccounts = new List<Task>();
+
+                //load seeds from Gmail
+                tasksAccounts.Add(Task.Run(() => LoadSeeds(gmailSeeds)));
+
+                //load seeds from Outlook
+                tasksAccounts.Add(Task.Run(() => LoadSeeds(outlookSeeds)));
+
+                //load seeds from AOL
+                tasksAccounts.Add(Task.Run(() => LoadSeeds(aolSeeds)));
+
+                //load seeds from Yahoo
+                tasksAccounts.Add(Task.Run(() => LoadSeeds(yahooSeeds)));
+
+                await Task.WhenAll(tasksAccounts);
+
+            }
+
+            catch (Exception ex)
+            {
+                var ravenClient = Sentry.Instance;
+                ravenClient.Capture(new SentryEvent(ex));
+            }
+        }
+
+        public async Task LoadSeeds(List<Seed> seeds)
+        {
+            while (seeds.Count() > 0)
+            {
+                var seed = seeds[0];
+
+                //log
+                Logger logger = LogManager.GetCurrentClassLogger();
+                logger.Info(("---Seed remaining to load " + seed.Provider.Name + ":" + seeds.Count()));
+
+                seeds.RemoveAt(0);
+                await LoadSeed(seed);
+            }
+        }
+
+        public async Task LoadSeed(Seed account)
+        {
+            String providerName = account.Provider.Name.ToString().ToLower();
+
+            MailAdapter.Dictionary.TryGetValue(providerName, out Dictionary<String, String> mailFolderMapper);
+
+            mailFolderMapper.TryGetValue("Inbox", out String inboxFolder);
+
+            mailFolderMapper.TryGetValue("Spam", out String spamFolder);
+
+            List<Task> tasksAccounts = new List<Task>();
+
+            if (inboxFolder != null && spamFolder != null)
+            {
+                //Read the Inbox folder on each client
+                tasksAccounts.Add(Task.Run(() => new MailClient().GetMessagesAsync(account, inboxFolder, "Inbox")));
+
+                //Read the Spam folder on each client
+                tasksAccounts.Add(Task.Run(() => new MailClient().GetMessagesAsync(account, spamFolder, "Spam")));
+
+            }
+            else
+            {
+                RavenClient ravenClient = Sentry.Instance;
+                ravenClient.Capture(new SentryEvent("There is no IMAP Client available!"));
+
+                MailStorage runLog = new MailStorage();
+                runLog.CreateRunLogEntry("Running", "End Load Seed: " + account.UserName);
+
+            }
+
+            await Task.WhenAll(tasksAccounts);
+        }
+
+    }
+}
---
+++ MailProcess/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using BusinessLogic.Model;
+using SharpRaven.Data;
+using System.Data.Entity;
+using NLog;
+
+namespace mail_app_process
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (var db = new DatabaseContext())
+                {
+                    var configuration = LogManager.Configuration;
+                    Logger logger = LogManager.GetCurrentClassLogger();
+                    logger.Info("Start Seeder");
+
+                    MailStorage runLog = new MailStorage();
+                    runLog.CreateRunLogEntry("Start", "Start to load mails");
+
+                    //Create list of seeds per provider
+                    List<Seed> gmailSeeds = new List<Seed>();
+                    List<Seed> outlookSeeds = new List<Seed>();
+                    List<Seed> aolSeeds = new List<Seed>();
+                    List<Seed> yahooSeeds = new List<Seed>();
+
+                    if (args.Length > 0)
+                    {
+                        foreach (var seedId in args)
+                        {
+                            Seed s = db.Seed.Find(Guid.Parse(seedId));
+
+                            if (s != null)
+                            {
+                                if (s.Provider.Name.ToLower() == "gmail")
+                                    gmailSeeds.Add(s);
+                                if (s.Provider.Name.ToLower() == "outlook")
+                                    outlookSeeds.Add(s);
+                                if (s.Provider.Name.ToLower() == "aol")
+                                    aolSeeds.Add(s);
+                                if (s.Provider.Name.ToLower() == "yahoo")
+                                    yahooSeeds.Add(s);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        gmailSeeds = db.Seed.Include(c => c.Provider).Where(p => p.Provider.Name.ToLower() == "gmail").ToList();
+
+                        outlookSeeds = db.Seed.Include(c => c.Provider).Where(p => p.Provider.Name.ToLower() == "outlook").ToList();
+
+                        aolSeeds = db.Seed.Include(c => c.Provider).Where(p => p.Provider.Name.ToLower() == "aol").ToList();
+
+                        yahooSeeds = db.Seed.Include(c => c.Provider).Where(p => p.Provider.Name.ToLower() == "yahoo").ToList();
+
+
+                    }
+                    AsyncController ac = new AsyncController();
+                    Task run = ac.AsyncLoadData(gmailSeeds, outlookSeeds, aolSeeds, yahooSeeds);
+
+                    run.Wait();
+                    logger.Info("End Seeder");
+                    runLog.CreateRunLogEntry("End", "End of messages load");
+                }
+            }
+            catch (Exception ex)
+            {
+                var ravenClient = Sentry.Instance;
+                ravenClient.Capture(new SentryEvent(ex));
+            }
+
+
+        }
+    }
+}
