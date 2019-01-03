dbcac254f126789fd70b21eeb5e6e9480947e835
Josh Greenberg <greenbergjosh@hotmail.com>
2019-01-03 14:27:26 -0500

    Interrupted work on GenericEntity

---
+++ SimpleImportExport/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleImportExport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
