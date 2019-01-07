4ff81cecd51ffaebd7293938662c50bc22e64f70
Alberto <abec@onpointglobal.com>
2019-01-07 15:16:54 -0500

    Minor refactor

---
+++ SimpleImportExport/Pattern.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using Utility;
+
+namespace SimpleImportExport
+{
+    public class Pattern
+    {
+        public Pattern(IGenericEntity ge)
+        {
+            Rx = new Regex(ge.GetS("Pattern"), RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            SourceRelativePath = ge.GetS("SrcPath");
+            DestinationRelativePath = ge.GetS("DestPath");
+        }
+
+        public Regex Rx { get; }
+        public string SourceRelativePath { get; }
+        public string DestinationRelativePath { get; }
+    }
+}
