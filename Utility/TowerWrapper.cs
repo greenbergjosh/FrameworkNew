63db5e4540c83739bfe6d2967d710dd14f137dba
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-01-09 16:02:36 -0800

    Utility/TowerWrapper.cs
    * Move logic out into a function so it can be used by debugging console
      (and whomever else)

--- Utility/TowerWrapper.cs
+++ Utility/TowerWrapper.cs
@@ -87,6 +87,13 @@ namespace Utility
             return towerEmailPlainText;
         }
 
+        public static string Base64FromEqs(string rawstring)
+        {
+           string[] a = rawstring.Split("  ");
+           a[a.Length - 1] = a[a.Length - 1].PadRight(a[a.Length - 1].Length + PadCount(a[a.Length - 1]), '=');
+           return String.Join("\r\n", a).Replace('-', '+').Replace('_', '/');
+        }
+
         public static async Task<(IGenericEntity opaque, string md5)> ProcessTowerMessage(FrameworkWrapper fw, HttpContext context, string towerEncryptionKey)
         {
             // All this method does in call SaveSession() with the decrypted opaque parm
@@ -100,10 +107,7 @@ namespace Utility
 
             try
             {
-                string[] a = rawstring.Split("  ");
-                a[a.Length - 1] = a[a.Length - 1].PadRight(a[a.Length - 1].Length + PadCount(a[a.Length - 1]), '=');
-                pRawString = String.Join("\r\n", a);
-                pRawString = pRawString.Replace('-', '+').Replace('_', '/');
+                pRawString = Base64FromEqs(rawstring);
             }
             catch (Exception ex)
             {
