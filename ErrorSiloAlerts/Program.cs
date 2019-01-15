f5b063d971acf78c449541870c03157e69e6e849
Alberto <abec@onpointglobal.com>
2019-01-15 18:01:36 -0500

    ErrorSiloAlerts coded, need DB dev and testing

---
+++ ErrorSiloAlerts/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Utility;
+using Jw = Utility.JsonWrapper;
+
+namespace ErrorSiloAlerts
+{
+    class Program
+    {
+        private static FrameworkWrapper fw = null;
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                fw = new FrameworkWrapper();
+                var lastSeqIds = await SqlWrapper.SqlToGenericEntity("History", "LastSeqIds", "", "");
+                var emails = new List<MailMessage>();
+
+                emails.AddRange(await GetEmailAlertDescriptorEvents(lastSeqIds.GetS("EmailAlerts").ParseLong() ?? 0));
+
+                using (var smtp = new SmtpClient("", 0))
+                {
+                    smtp.EnableSsl = true;
+
+                    foreach (var m in emails)
+                    {
+                        try
+                        {
+                            smtp.Send(m);
+                        }
+                        catch (Exception e)
+                        {
+                            await fw.Err(ErrorSeverity.Error, nameof(Main), ErrorDescriptor.Exception, $"Email delivery failed: {e}");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                if (fw == null) Console.WriteLine($"Failed to load {nameof(FrameworkWrapper)}: {e}");
+                else await fw.Err(ErrorSeverity.Error, nameof(Main), ErrorDescriptor.Exception, e.ToString());
+            }
+        }
+
+        private static async Task<IEnumerable<MailMessage>> GetEmailAlertDescriptorEvents(long lastSeqId)
+        {
+            // ToDo: Refactor to do in a single pass
+            var config = fw.StartupConfiguration.GetE("Config/EmailAlerts");
+            var rawEmailAlerts = await SqlWrapper.SqlToGenericEntity("ErrorWarehouse", "EmailAlerts", Jw.Json(new { lastSeqId }), "");
+            var configGroup = new Dictionary<string, (IGenericEntity config, List<IGenericEntity> alerts)>();
+            var emails = new List<MailMessage>();
+
+            foreach (var alert in rawEmailAlerts.GetL(""))
+            {
+                var configName = alert.GetS("Method");
+                var alertConfig = config.GetE(configName);
+
+                if (alertConfig == null)
+                {
+                    configName = "Default";
+                    alertConfig = config.GetE(configName);
+                }
+
+                if (!configGroup.ContainsKey(configName)) configGroup.Add(configName, (config: alertConfig, alerts: new List<IGenericEntity>()));
+
+                configGroup[configName].alerts.Add(alert);
+            }
+
+            foreach (var pgk in configGroup.Keys)
+            {
+                var pg = configGroup[pgk];
+                var dataGroupPath = pg.config.GetS("GroupBy") ?? "Method";
+                string GetDataGroup(IGenericEntity e) => e.GetS(dataGroupPath);
+                var dataGroups = new Dictionary<string, (string body, List<string> items)>();
+                var bodyTemplate = pg.config.GetS("BodyTemplate");
+                var itemTemplate = pg.config.GetS("BodyItemTemplate");
+                var itemFailedTemplate = pg.config.GetS("BodyFailedItemTemplate");
+                var from = pg.config.GetS("From");
+                var to = pg.config.GetS("To");
+                var subject = pg.config.GetS("Subject");
+
+                foreach (var alert in pg.alerts)
+                {
+                    var dataGroup = GetDataGroup(alert);
+
+                    if (!dataGroups.ContainsKey(dataGroup)) dataGroups.Add(dataGroup, (body: bodyTemplate.Replace("Group", dataGroup), items: new List<string>()));
+
+                    if (alert.GetS("ErrorMsg").IsNullOrWhitespace()) dataGroups[dataGroup].items.Add(ReplaceTokens(itemTemplate, alert));
+                    else dataGroups[dataGroup].items.Add(ReplaceTokens(itemFailedTemplate, alert));
+                }
+
+                var body = dataGroups.Values.Aggregate("",
+                    (s, dataGroup) =>
+                    {
+                        s += dataGroup.body.Replace("[Items]", dataGroup.items.Join(""));
+                        return s;
+                    });
+
+                emails.Add(new MailMessage(from, to, subject, body));
+            }
+
+            return emails;
+        }
+
+        private static string ReplaceTokens(string template, IGenericEntity entity)
+        {
+            foreach (var key in entity.GetD("").Select(p => p.Item1))
+            {
+                template = template.Replace($"[{key}]", entity.GetS(key));
+            }
+
+            return template;
+        }
+
+    }
+}
--- SystemHealthChecks/Program.cs
+++ SystemHealthChecks/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Mail;
 using System.Runtime.InteropServices;
-using System.Security;
-using Microsoft.Management.Infrastructure;
-using Microsoft.Management.Infrastructure.Options;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Utility;
+using Jw = Utility.JsonWrapper;
 
 namespace SystemHealthChecks
 {
@@ -15,108 +13,65 @@ namespace SystemHealthChecks
     {
         private static FrameworkWrapper fw = null;
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
                 fw = new FrameworkWrapper();
 
-                var relay = fw.StartupConfiguration.GetS("Config/SmtpRelay");
-                var port = fw.StartupConfiguration.GetS("Config/SmtpPort").ParseInt();
-                var to = fw.StartupConfiguration.GetS("Config/AlertEmailTo");
-                var from = fw.StartupConfiguration.GetS("Config/AlertEmailFrom");
+                var alertPayload = LowDiskSpaceChecks();
 
-                if (!port.HasValue || new[] { relay, to, from }.Any(s => s.IsNullOrWhitespace()))
+                if (!alertPayload.IsNullOrWhitespace())
                 {
-                    fw.Err(ErrorSeverity.Error, nameof(Main), ErrorDescriptor.Exception, $"Invalid config: {fw.StartupConfiguration.GetS("")}");
-                    return;
-                }
-
-                var sslEnabled = fw.StartupConfiguration.GetS("Config/DisableSSL").ParseBool() != true;
-                var smtpCredsUser = fw.StartupConfiguration.GetS("Config/SmtpCredentials/User");
-                var smtpCredsPassword = fw.StartupConfiguration.GetS("Config/SmtpCredentials/Password");
-                var alert = args.FirstOrDefault()?.ToLower() == "test" ? (subject: "Email alert test", body: "Email alert test") : LowDiskSpaceChecks();
-
-                if (!alert.subject.IsNullOrWhitespace())
-                {
-                    using (var smtp = new SmtpClient(relay, port.Value))
-                    {
-                        if (sslEnabled) smtp.EnableSsl = true;
-
-                        if (smtpCredsUser != null)
-                        {
-                            smtp.Credentials = new NetworkCredential(smtpCredsUser, smtpCredsPassword);
-                        }
-
-                        smtp.Send(new MailMessage(from, to)
-                        {
-                            IsBodyHtml = true,
-                            Subject = alert.subject,
-                            Body = $"<html>{alert.body}</html>"
-                        });
-                    }
+                    await fw.Err(ErrorSeverity.Alert, nameof(LowDiskSpaceChecks), ErrorDescriptor.EmailAlert, alertPayload);
                 }
             }
             catch (Exception e)
             {
                 if (fw == null) Console.WriteLine($"Failed to load {nameof(FrameworkWrapper)}: {e}");
-                else fw.Err(ErrorSeverity.Error, nameof(Main), ErrorDescriptor.Exception, e.ToString());
+                else await fw.Err(ErrorSeverity.Error, nameof(Main), ErrorDescriptor.Exception, e.ToString());
             }
         }
 
-        private static (string subject, string body) LowDiskSpaceChecks()
+        private static string LowDiskSpaceChecks()
         {
-            var alerts = new List<string>();
+            var alerts = new List<EmailAlertPayloadItem>();
             var config = fw.StartupConfiguration.GetE("Config/LowDiskSpace");
-            var threshold = config.GetS("PercentThreshold").ParseUInt();
+            var thresholdStr = config.GetS("PercentThreshold");
+            var threshold = thresholdStr.ParseUInt();
             var drives = config.GetD("Drives");
-            var itemTemplate = config.GetS("EmailBodyItemTemplate");
-            var failedItemTemplate = config.GetS("EmailBodyFailedItemTemplate");
-            var subject = config.GetS("EmailSubject");
-            var body = config.GetS("EmailBody");
 
-            if (threshold.HasValue &&
-                new[] { itemTemplate, failedItemTemplate, subject, body }.All(s => !s.IsNullOrWhitespace()))
+            if (threshold.HasValue)
             {
                 foreach (var drive in drives)
                 {
-                    var stats = GetDriveUsageDetails(drive.Item2);
-
-                    if (stats.capacity == 0)
+                    try
                     {
-                        alerts.Add(
-                            failedItemTemplate.Replace("[label]", drive.Item1)
-                                .Replace("[error]", "Drive unreachable"));
-                    }
-                    else
-                    {
-                        var percFree = stats.free * 100 / stats.capacity;
+                        var stats = GetLocalDriveDetails(drive.Item2);
 
-                        if (percFree < threshold)
+                        if (stats.capacity == 0) alerts.Add(new DiskSpaceDetails(drive.Item1, "Drive unreachable"));
+                        else
                         {
-                            alerts.Add(
-                                itemTemplate.Replace("[label]", drive.Item1)
-                                    .Replace("[free]", stats.free.ToString())
-                                    .Replace("[used]", stats.used.ToString())
-                                    .Replace("[capacity]", stats.capacity.ToString())
-                                    .Replace("[percentFree]", percFree.ToString()));
+                            var percFree = stats.free * 100 / stats.capacity;
+
+                            if (percFree < threshold)
+                            {
+                                alerts.Add(new DiskSpaceDetails(drive.Item1, Convert.ToInt32(percFree), Convert.ToInt32(stats.free / 1024), Convert.ToInt32(stats.used / 1024), Convert.ToInt32(stats.capacity / 1024)));
+                            }
                         }
+                    }
+                    catch (Exception e)
+                    {
+                        alerts.Add(new DiskSpaceDetails(drive.Item1, $"Failed to retrieve data: {e}"));
                     }
                 }
             }
             else
             {
-                subject = "Invalid LowDiskSpace config";
-                alerts.Add(config.GetS(""));
-                fw.Err(ErrorSeverity.Error, nameof(LowDiskSpaceChecks), ErrorDescriptor.Exception, $"Invalid LowDiskSpace config: {config.GetS("")}");
+                alerts.Add(new EmailAlertPayloadItem("Invalid Config", $"PercentThreshold missing or not valid string: PercentThreshold={thresholdStr.IfNullOrWhitespace("null")}"));
             }
 
-            if (alerts.Any())
-            {
-                return (subject: subject, body: body.Replace("[items]", alerts.Join("")).Replace("[percentThreshold]", threshold.ToString()));
-            }
-
-            return (null, null);
+            return alerts.Any() ? PL.N("Config", config.GetS("")).Add(PL.N("Alerts", JsonConvert.SerializeObject(alerts))).ToString() : null;
         }
 
         [DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Auto)]
@@ -126,44 +81,33 @@ namespace SystemHealthChecks
             out ulong lpTotalNumberOfBytes,
             out ulong lpTotalNumberOfFreeBytes);
 
-        private static (ulong free, ulong used, ulong capacity) GetDriveUsageDetails1(string path)
+        private static (ulong free, ulong used, ulong capacity) GetLocalDriveDetails(string path)
         {
             const ulong div = 1048576L; // Math.Pow(1024,2)
             var d = GetDiskFreeSpaceEx(path, out var freeBytes, out var totalBytes, out var totalFreeBytes);
 
             if (totalBytes == 0) return (0, 0, 0);
-            var dd = (totalBytes - freeBytes) / div;
-
-            ulong bytesToMegs(ulong byteSize)
-            {
-                return (byteSize / div);
-            }
 
             return (free: freeBytes / div, used: (totalBytes - freeBytes) / div, capacity: totalBytes / div);
         }
 
-        private static (ulong free, ulong used, ulong capacity) GetDriveUsageDetails(string path)
+    }
+
+    public class DiskSpaceDetails : EmailAlertPayloadItem
+    {
+        public DiskSpaceDetails(string drive, string errorMsg) : base(drive, errorMsg) { }
+
+        public DiskSpaceDetails(string drive, int freePercent, int freeGB, int usedGB, int capacityGB) : base(drive, null)
         {
-            return (0, 0, 0);
-            string computer = "Computer_B";
-            string domain = "Domain1";
-            string username = "User1";
-
-            var plaintextpassword = "";
-            var securepassword = new SecureString();
-
-            foreach (var c in plaintextpassword)
-            {
-                securepassword.AppendChar(c);
-            }
-
-            CimCredential Credentials = new CimCredential(PasswordAuthenticationMechanism.Default, domain, username, securepassword);
-
-            WSManSessionOptions SessionOptions = new WSManSessionOptions();
-            SessionOptions.AddDestinationCredentials(Credentials);
-
-            CimSession Session = CimSession.Create(computer, SessionOptions);
+            FreePercent = freePercent;
+            FreeGB = freeGB;
+            UsedGB = usedGB;
+            CapacityGB = capacityGB;
         }
 
+        public int FreePercent { get; }
+        public int FreeGB { get; }
+        public int UsedGB { get; }
+        public int CapacityGB { get; }
     }
 }
