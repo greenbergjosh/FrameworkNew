04d48dc296b81f1882e36af8a6c3d9cfe5071a69
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-02-20 15:19:55 -0800

    [Utility/HttpContextExt.cs] conditionally add CORS headers
    * Based on a passed-in configuration, add in the CORS headers
      which were previously handled by middle-ware

--- Utility/HttpContextExt.cs
+++ Utility/HttpContextExt.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Linq;
 
 namespace Utility
 {
@@ -101,17 +102,58 @@ namespace Utility
             await ctx.Response.WriteAsync(response);
         }
 
-        public static void AddCorsAccessForOriginHost(this HttpContext ctx)
+        public static void AddCorsAccessForOriginHost(this HttpContext ctx, IGenericEntity ge)
         {
             // https://docs.microsoft.com/en-us/aspnet/core/migration/21-to-22?view=aspnetcore-2.2&tabs=visual-studio
             // .netcore 2.2 makes this process more explicit (domains should be specified), so let's just echo the
             // requesting origin back, circumventing the problem. Feel free to add other methods as necessary below.
-            if (ctx.Request.Headers.ContainsKey("Origin"))
+
+            // Nothing to do if we don't have an "Origin:" header or if our config is missing
+            // the "allow-origin" directive
+            if (! ctx.Request.Headers.TryGetValue("Origin", out StringValues originHost) ||
+                ge.GetS("allow-origin") == null
+                ) return;
+
+            // "allow-origin": [ "*" ]
+            //
+            // "allow-origin": [ "https://example1.com","http://example2.com" ]
+            //
+            // When set to "*", add the request's "Origin:" header into the
+            // response via the "Access-Control-Allow-Origin:" header
+            //
+            // Acts as a white-list when set to an explicit list of URLs, add
+            // the the request's "Origin:" header into the response via the
+            // "Access-Control-Allow-Origin:" header if the "Origin:" request
+            // header matches a URL in the list.
+            if (ge.GetS("allow-origin[0]") == "*")
             {
-                ctx.Request.Headers.TryGetValue("Origin", out StringValues originHost);
                 ctx.Response.Headers.Add("Access-Control-Allow-Origin", originHost);
-                ctx.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
-                ctx.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+            }
+            else if (ge.GetL("allow-origin").Select(x => x.GetS("") == originHost).Count() == 1)
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Origin", originHost);
+            }
+            // "allow-headers": [ "origin", "x-requested-with", "access-control-request-headers", "content-type", "access-control-request-method", "accept" ],
+            //
+            // Comma-separates a list of values into the "Access-Control-Allow-Headers" response header
+            if (ge.GetS("allow-headers") != null)
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Headers",string.Join(",", ge.GetL("allow-headers").Select(x => x.GetS(""))));
+            }
+            // "allow-methods": [ "PUT", "POST", "GET", "PATCH", "HEAD" ],
+            //
+            // Comma-separates a list of values into the "Access-Control-Allow-Methods" response header
+            if (ge.GetS("allow-methods") != null)
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Methods",string.Join(",", ge.GetL("allow-methods").Select(x => x.GetS(""))));
+            }
+
+            // "allow-credentials": "true"
+            //
+            // Sets the "Access-Control-Allow-Credentials" response value to a bool to indicate cross-site cookie policy
+            if (ge.GetS("allow-credentials") != null)
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Credentials",ge.GetS("allow-credentials"));
             }
         }
 
