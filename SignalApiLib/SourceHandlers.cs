deceb9003c29985828748563bd70a75203cd80da
Alberto <qbanbpc@gmail.com>
2019-02-22 15:50:39 -0500

    SignalAPi Fluent post to Console

--- SignalApiLib/SourceHandlers.cs
+++ SignalApiLib/SourceHandlers.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -9,6 +12,9 @@ namespace SignalApiLib
     public class Fluent
     {
         private readonly FrameworkWrapper _fw;
+        private const string Key = "498937C1-6FCA-45B6-9C86-49D4999BB5C7";
+        private const string ConsoleUrl = "https://forms.direct-market.com/PostExtra/pe";
+        private readonly string _logCtx = $"{nameof(Fluent)}.{nameof(HandleRequest)}";
 
         public Fluent(FrameworkWrapper fw)
         {
@@ -17,23 +23,85 @@ namespace SignalApiLib
 
         public async Task<string> HandleRequest(string requestFromPost, HttpContext ctx)
         {
-            var js = JsonConvert.DeserializeObject(requestFromPost);
+            var body = JsonConvert.DeserializeObject(requestFromPost);
 
-            if (js is JObject) requestFromPost = $"[{requestFromPost}]";
+            if (body is JObject b)
+            {
+                if (b["k"].ToString() != Key) return null;
 
-            var res = await SqlWrapper.SqlToGenericEntity("Fluent", "SaveData", "", requestFromPost);
+                var token = b["p"];
+                JArray payload;
+                Task consoleTask;
 
-            if (res.GetS("Result") == "Success") return res.GetS("");
+                if (token is JObject)
+                {
+                    payload = new JArray { token };
+                    consoleTask = PostToConsole((JObject)token);
+                }
+                else if (token is JArray)
+                {
+                    payload = (JArray)token;
+                    consoleTask = PostToConsole(payload);
+                }
+                else
+                {
+                    await _fw.Error(_logCtx, $"Invalid payload {token}");
+                    return null;
+                }
 
-            await _fw.Error($"{nameof(Fluent)}", $"DB failure. Response: {res.GetS("")}" );
+                var localDbTask = SqlWrapper.SqlToGenericEntity("Fluent", "SaveData", "", payload.ToString());
+
+                await Task.WhenAll(localDbTask, consoleTask);
+
+                if (localDbTask.Result.GetS("Result") == "Success") { return localDbTask.Result.GetS(""); }
+
+                await _fw.Error(_logCtx, $"DB failure. Response: {localDbTask.Result.GetS("")}");
+            }
+            else await _fw.Error(_logCtx, $"Invalid json body: {requestFromPost}");
 
             return null;
         }
 
-        //private string QueryStringToJson(IQueryCollection query)
-        //{
-        //    return JsonConvert.SerializeObject(query.Where(q => q.Key != "m").ToDictionary(q => q.Key, q => q.Value.LastOrDefault()));
-        //}
+        private async Task PostToConsole(JObject payload)
+        {
+            var email = payload["em"]?.ToString();
+
+            if (email.IsNullOrWhitespace()) return;
+
+            try
+            {
+                await ProtocolClient.HttpPostAsync(ConsoleUrl,
+                    JsonConvert.SerializeObject(new
+                    {
+                        header = new { svc = 1, p = -1 },
+                        body = new
+                        {
+                            domain_id = "1d6b7dd9-6d97-44b8-a795-9d0e5e72a01f",
+                            domain = "fluent feed",
+                            isFinal = true,
+                            //url = null,
+                            //phone_home = null,
+                            email
+                        }
+                    }), "application/json");
+            }
+            catch (Exception e)
+            {
+                await _fw.Error($"{_logCtx}.{nameof(PostToConsole)}", $"Failed to post {payload} to Console: {e.UnwrapForLog()}");
+            }
+        }
+
+        private async Task PostToConsole(JArray payload)
+        {
+            try
+            {
+                await payload.AsJEnumerable().Select(p=>(JObject)p).ForEachAsync(3, PostToConsole);
+            }
+            catch (Exception e)
+            {
+                await _fw.Error($"{_logCtx}.{nameof(PostToConsole)}[]", $"Failed to post {payload} to Console: {e.UnwrapForLog()}");
+            }
+        }
 
     }
 }
