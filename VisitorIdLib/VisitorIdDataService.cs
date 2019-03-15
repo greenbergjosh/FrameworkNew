c8cb718a37e6d3ff9f9cb37aaa2bdf3ba0a5c81e
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-03-15 08:03:20 -0700

    [VisitorIdDataService.cs] use string constant

--- VisitorIdLib/VisitorIdDataService.cs
+++ VisitorIdLib/VisitorIdDataService.cs
@@ -887,7 +887,7 @@ namespace VisitorIdLib
         {
             var header = Jw.Json(new { svc = 1, page = -1 }, new bool[] { false, false });
             var result = await Data.CallFnString("VisitorId", "LookupLeadByMd5", Jw.Json(new { md5 = md5 }), "{}",this.SqlTimeoutSec);
-            if (result == "{}") // TODO: remove and use Alberto's constant (awaiting merge)
+            if (result == Jw.Empty)
             {
                 await Fw.Log(nameof(PostMd5LeadDataToConsole), $"Unable to find adequate lead data for md5: {md5} from pid: {provider}");
                 return;
