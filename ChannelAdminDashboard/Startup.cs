b6ea8689f8033d8623a08f86f642c3954720f7aa
Patrick Eisenmann <patrickeisenmann@patricks-mbp-2.attlocal.net>
2019-03-26 08:21:45 -0400

    Initial Commit of ChannelAdminDashboard

---
+++ ChannelAdminDashboard/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
+
+namespace OnPoint.Channel.Admin
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName)
+				.AddChakraCore();
+
+			services.AddReact();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+			// Build the intermediate service provider then return it
+			services.BuildServiceProvider();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseStaticFiles();
+
+			// Initialise ReactJS.NET. Must be before static files.
+			app.UseReact(config =>
+			{
+				config
+					.SetReuseJavaScriptEngines(true)
+					.SetLoadBabel(true)
+					.SetLoadReact(true)
+					//.AddScript("../Content/components/index.js")
+					.AddScriptWithoutTransform("~/dist/runtime.js")
+					.AddScriptWithoutTransform("~/dist/vendor.js")
+					.AddScriptWithoutTransform("~/dist/components.js");
+			});
+
+			// TODO: Remove the MVC
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute("default", "{path?}", new { controller = "Dashboard", action = "Index" });
+				routes.MapRoute("lbm", "lbm/{id?}", new { controller = "Dashboard", action = "LBM" });
+			});
+		}
+	}
+}
