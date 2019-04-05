9517e149786c51e8240d092c436d18e1c7303ecf
Alberto <qbanbpc@gmail.com>
2019-04-05 18:42:33 -0400

    Utility.Auth init

---
+++ Utility.OpgAuth/Auth.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Utility.DataLayer;
+
+namespace Utility.OpgAuth
+{
+    public static class Auth
+    {
+        private const string Conn = "FW_AuthServer";
+        private static string _initError = "Auth not configured";
+        private static bool _initialized = false;
+        private static FrameworkWrapper _fw = null;
+
+        public static async Task Initialize(FrameworkWrapper fw)
+        {
+            try
+            {
+                _fw = fw;
+                var conf = GetConfig(false);
+                var conn = conf?.GetS("Conn").ParseGuid();
+
+                if (conf != null)
+                {
+                    if (!conn.HasValue) _initError = "";
+                    else
+                    {
+                        await Data.AddConnectionStrings(new[] {new Tuple<string, string>(Conn, conn.Value.ToString())});
+                        // ToDo: Initialize 3rd Party Platforms
+                        _initialized = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _initError = $"Unhandled Auth init exception: {e.UnwrapForLog()}";
+            }
+            
+            if(!_initialized) throw new Exception(_initError);
+        }
+
+        public static async Task DoSomething()
+        {
+            if(!_initialized) throw new Exception(_initError);
+            var conn = Conn;
+        }
+
+        private static IGenericEntity GetConfig(bool throwOnNull = true)
+        {
+            var conf = _fw?.StartupConfiguration.GetE("OpgAuth");
+
+            if(conf == null && throwOnNull) throw new Exception(_initError);
+
+            return conf;
+        }
+    }
+}
---
+++ Utility.OpgAuth/Sso/UserDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace Utility.OpgAuth.Sso
+{
+    public class UserDetails
+    {
+        public UserDetails(string name, string email, string phone, string imageUrl, string raw)
+        {
+            Name = name;
+            Email = email;
+            Phone = phone;
+            ImageUrl = imageUrl;
+            Raw = raw;
+        }
+
+        public string Name { get; }
+        public string Email { get; }
+        public string Phone { get; }
+        public string ImageUrl { get; }
+        public string Raw { get; }
+    }
+}
