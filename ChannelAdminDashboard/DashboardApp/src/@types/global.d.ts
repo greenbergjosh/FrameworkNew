a6034ff681daad21494398e377a8f2e2e363f713
Derrick Beining <derrick.beining@gmail.com>
2019-04-08 10:05:57 -0400

    [ChannelAdmin] global.d.ts, clean up

--- ChannelAdminDashboard/DashboardApp/src/@types/global.d.ts
+++ ChannelAdminDashboard/DashboardApp/src/@types/global.d.ts
@@ -1,14 +1,9 @@
 declare type Nil = undefined | null
 declare type Nilable<T> = T | null | undefined
-/** From T, remove keys in K */
-declare type Required<T> = T extends object ? { [P in keyof T]-?: NonNullable<T[P]> } : T
-declare type DeepRequired<T, U extends object | undefined = undefined> = T extends object
-  ? {
-      [P in keyof T]-?: NonNullable<T[P]> extends NonNullable<U | Function | Class>
-        ? NonNullable<T[P]>
-        : DeepRequired<NonNullable<T[P]>, U>
-    }
-  : T
 declare type PropsFromComponent<
   C extends React.ComponentType<any>
 > = C extends React.ComponentType<infer P> ? P : never
+
+/** Make keys in K required in T */
+declare type Require<T extends object, K extends keyof T> = Pick<T, Exclude<keyof T, K>> &
+  Required<Pick<T, K>>
