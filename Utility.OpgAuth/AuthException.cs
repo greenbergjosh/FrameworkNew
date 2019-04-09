ea9a80b45e4722253c471c52b3d05e4ce531d919
Alberto <qbanbpc@gmail.com>
2019-04-09 12:09:56 -0400

    Auth framework and SSO ready for testing

---
+++ Utility.OpgAuth/AuthException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Utility.OpgAuth
+{
+    public class AuthException : Exception
+    {
+        public AuthException(string msg) : base(msg)
+        {
+
+        }
+    }
+
+    public class AuthFrameworkNotFoundException : Exception
+    {
+        public AuthFrameworkNotFoundException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
---
+++ Utility.OpgAuth/Sso/Platform.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Utility.OpgAuth.Sso
+{
+    public abstract class Platform
+    {
+        public abstract void Init(FrameworkWrapper fw, IGenericEntity init);
+
+        public abstract string PlatformType { get; }
+
+        public abstract Task<UserDetails> GetUserDetails(IGenericEntity authData);
+    }
+}
