bed84427c1fb02108ac5b47f4fea18ff4cbfad34
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-04-12 13:24:39 -0700

    [VisitorIdResponse.cs] Add CookieData as a property

--- VisitorIdLib/VisitorIdResponse.cs
+++ VisitorIdLib/VisitorIdResponse.cs
@@ -6,17 +6,19 @@ namespace VisitorIdLib
 {
     public class VisitorIdResponse
     {
-        public VisitorIdResponse(string result, string md5, string email, string sid)
+        public VisitorIdResponse(string result, string md5, string email, string sid, CookieData cookieData)
         {
             Result = result;
             Md5 = md5;
             Email = email;
             Sid = sid;
+            CookieData = cookieData;
         }
 
         public string Result { get; }
         public string Md5 { get; }
         public string Email { get; }
         public string Sid { get; }
+        public CookieData CookieData { get; }
     }
 }
