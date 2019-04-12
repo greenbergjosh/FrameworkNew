5742a79a79d55d58214ab72b23ecce9ec8866915
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-04-12 13:09:03 -0700

    [CookieData.cs, Visit.cs] new classes
    * Visit.cs - Multiple classes: Visit, DomainVisit, PageVisit.  These
      classes contain detail regarding a visit to their respective
      things (a Visitor Id pixel, a domain, and a page on a domain).
    
    * CookieData.cs - maintains the deserialized state of a visit to a page,
      including the Visit classes above, as well as some other details
      related to recency of visit, and Md5Provider responses

---
+++ VisitorIdLib/Visit.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace VisitorIdLib
+{
+    public class Visit
+    {
+        [JsonProperty(PropertyName = "Id")]
+        public Guid ReportingSequenceId { get; set; }
+        public string Domain { get; set; }
+        public DateTime VisitDateTime { get; set; }
+
+        [JsonIgnore]
+        public string Page { get; set; }
+        [JsonIgnore]
+        public bool VeryFirstVisit;
+        [JsonIgnore]
+        public Guid RsConfigId { get; set; }
+        [JsonIgnore]
+        public string ReportingSequenceName;
+        [JsonIgnore]
+        public string EventName => ReportingSequenceName + "SessionInitiate";
+
+        public Visit()
+        {
+            this.ReportingSequenceName = "VisitorId";
+        }
+
+        public Visit( bool veryFirstVisit, DateTime visitDateTime, string domain, string page, Guid rsConfigId, Guid reportingSequenceId ) : this()
+        {
+            VeryFirstVisit = veryFirstVisit;
+            VisitDateTime = visitDateTime;
+            Domain = domain;
+            Page = page;
+            RsConfigId = rsConfigId;
+            ReportingSequenceId = reportingSequenceId;
+        }
+
+        public EdwBulkEvent ReportingSequenceEvent (PL addlPayLoad = null)
+        {
+            var be = new EdwBulkEvent();
+            be.AddRS(EdwBulkEvent.EdwType.Immediate, this.ReportingSequenceId, this.VisitDateTime, addlPayLoad == null ? ReportingSequencePayload() : ReportingSequencePayload().Add(addlPayLoad), this.RsConfigId);
+            return be;
+        }
+
+        virtual public PL ReportingSequencePayload()
+        {
+            return PL.O(new { this.VisitDateTime, this.Domain, this.Page });
+        }
+
+        public EdwBulkEvent VisitEvent(PL payload, Dictionary<string, object> siblingRsids)
+        {
+            var be = new EdwBulkEvent();
+            PL eventPayload = PL.O(new { et = this.EventName });
+            eventPayload.Add(payload);
+            be.AddEvent(Guid.NewGuid(), DateTime.UtcNow, siblingRsids, null, eventPayload);
+
+            return be;
+        }
+
+
+    }
+
+    public class DomainVisit : Visit
+    {
+
+        public int VisitNum { get; set; }
+
+        [JsonIgnore]
+        public DateTime? ExpiredVisitDateTime { get; set; }
+        [JsonIgnore]
+        public int? ExpiredVisitNum { get; set; }
+        [JsonIgnore]
+        public Guid? ExpiredReportingSequenceId { get; set; }
+
+        public DomainVisit() : base()
+        {
+            this.ReportingSequenceName = "Domain";
+        }
+
+        public DomainVisit( DateTime visitDateTime, string domain, string page, Guid rsConfigId, Guid reportingSequenceId) : this()
+        {
+            this.VisitDateTime = visitDateTime;
+            this.Domain = domain;
+            this.Page = page;
+            this.RsConfigId = rsConfigId;
+            this.ReportingSequenceId = reportingSequenceId;
+        }
+
+        public bool IsExpired (DateTime timeOfCurentVisit, TimeSpan sessionDuration)
+        {
+            // VisitDateTime is the time of the *last* visit.  This method is
+            // to be called before the object has had its visit time updated to the
+            // current visit time.
+            return this.VisitDateTime.Add(sessionDuration) < timeOfCurentVisit;
+        }
+
+        public void Expire (DateTime newVisitDateTime)
+        {
+
+            this.ExpiredVisitDateTime = this.VisitDateTime;
+            this.ExpiredVisitNum = this.VisitNum;
+            this.ExpiredReportingSequenceId = this.ReportingSequenceId;
+            this.VisitNum = 1;
+            this.VisitDateTime = newVisitDateTime;
+            this.ReportingSequenceId = Guid.NewGuid();
+        }
+
+        public void UpdateVisitStats (DateTime newVisitDateTime)
+        {
+            this.VisitNum++;
+            this.VisitDateTime = newVisitDateTime;
+        }
+
+        override public PL ReportingSequencePayload()
+        {
+            return this.ExpiredVisitDateTime == null ? base.ReportingSequencePayload() :
+                base.ReportingSequencePayload().Add(PL.O(new { this.ExpiredVisitDateTime, this.ExpiredVisitNum, this.ExpiredReportingSequenceId } ));
+        }
+
+    }
+
+    public class PageVisit : DomainVisit
+    {
+        public PageVisit() : base()
+        {
+            this.ReportingSequenceName = "Page";
+        }
+
+        public PageVisit(DateTime visitDateTime, string domain, string page, Guid rsConfigId, Guid reportingSequenceId) : this()
+        {
+            this.VisitDateTime = visitDateTime;
+            this.Domain = domain;
+            this.Page = page;
+            this.RsConfigId = rsConfigId;
+            this.ReportingSequenceId = reportingSequenceId;
+        }
+
+        override public PL ReportingSequencePayload()
+        {
+            return PL.O(new { this.VisitDateTime, this.Domain, this.Page, this.VisitNum });
+        }
+
+    }
+
+
+}
