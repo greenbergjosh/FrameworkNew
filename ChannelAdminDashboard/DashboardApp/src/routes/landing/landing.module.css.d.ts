112e237f14b53d660600b8df6ecbb657e12dcfe6
Derrick Beining <derrick.beining@gmail.com>
2019-04-19 13:13:06 -0400

    [ChannelAdmin] add node-sass to devDeps

--- ChannelAdminDashboard/DashboardApp/src/routes/landing/landing.module.css.d.ts
+++ ChannelAdminDashboard/DashboardApp/src/routes/landing/landing.module.css.d.ts
@@ -1,13 +1,14 @@
 declare const styles: {
-  readonly image: string
-  readonly fullHeight: string
-  readonly sidebar: string
-  readonly loginFormArea: string
-  readonly loginFormForgot: string
-  readonly loginFormButton: string
-  readonly sidebarRightContent: string
-  readonly title: string
-  readonly welcomeBackArea: string
-  readonly welcomeBackText: string
-}
-export = styles
+  readonly "image": string;
+  readonly "fullHeight": string;
+  readonly "sidebar": string;
+  readonly "loginFormArea": string;
+  readonly "loginFormForgot": string;
+  readonly "loginFormButton": string;
+  readonly "sidebarRightContent": string;
+  readonly "title": string;
+  readonly "welcomeBackArea": string;
+  readonly "welcomeBackText": string;
+};
+export = styles;
+
