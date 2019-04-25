e8d870fdd78b9ac16589cc1a9bcff21f54382a7e
Patrick Eisenmann <peisenmann@onpointglobal.com>
2019-04-25 09:58:56 -0400

    [ChannelAdmin] Form Code Editor and Initial Reporting stuff

--- DashboardApp/src/routes/dashboard/routes/reports/index.tsx
+++ DashboardApp/src/routes/dashboard/routes/reports/index.tsx
@@ -4,10 +4,5 @@ import { WithRouteProps } from "../../../../state/navigation"
 interface Props {}
 
 export function Reports(props: WithRouteProps<Props>): JSX.Element {
-  return (
-    <div>
-      <div>Reports</div>
-      {props.children}
-    </div>
-  )
+  return <>{props.children}</>
 }
