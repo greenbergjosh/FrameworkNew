f180166ea0d0b8e5842db020b8cb71329e5e71aa
Alberto <qbanbpc@gmail.com>
2019-04-29 13:00:13 -0400

    Simplifications to ease UI dev

--- TheGreatWallOfDataLib/Scopes/Config.cs
+++ TheGreatWallOfDataLib/Scopes/Config.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Utility;
 using Utility.DataLayer;
 using Jw = Utility.JsonWrapper;
@@ -14,8 +15,9 @@ namespace TheGreatWallOfDataLib.Scopes
         public static async Task<IGenericEntity> Merge(string connName, string payload, string identity)
         {
             var ids = JsonConvert.DeserializeObject<string[]>(payload);
+            var res = await Data.GetConfigs(ids, null, connName, "_selectConf");
 
-            return Jw.JsonToGenericEntity(await Data.GetConfigs(ids, null, connName, "_selectConf"));
+            return Jw.ToGenericEntity(new { r = 0, result = Jw.TryParseObject(res) });
         }
     }
 }
