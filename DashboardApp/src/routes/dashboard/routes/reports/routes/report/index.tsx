e988c736af1ef7498d83b9622327f902188fe298
Derrick Beining <derrick.beining@gmail.com>
2019-04-30 15:27:56 -0400

    [ChannelAdmin]: reports WIP, io-ts types etc

--- DashboardApp/src/routes/dashboard/routes/reports/routes/report/index.tsx
+++ DashboardApp/src/routes/dashboard/routes/reports/routes/report/index.tsx
@@ -14,16 +14,20 @@ import {
 } from "@syncfusion/ej2-react-grids"
 import { Button, Select, Typography } from "antd"
 import { Do } from "fp-ts-contrib/lib/Do"
-import { tryCatch2v } from "fp-ts/lib/Either"
+import { tryCatch2v, fromOption as eitherFromOption } from "fp-ts/lib/Either"
+import { Identity } from "fp-ts/lib/Identity"
+import * as these from "fp-ts/lib/These"
 import { identity } from "fp-ts/lib/function"
-import { fromEither, option } from "fp-ts/lib/Option"
+import { fromEither as optionFromEither, option, none } from "fp-ts/lib/Option"
 import * as record from "fp-ts/lib/Record"
 import React, { RefObject, useRef } from "react"
 import { None, Some } from "../../../../../../data/Option"
-import { QueryConfig, ReportConfig } from "../../../../../../data/Report"
 import { useRematch } from "../../../../../../hooks"
 import { WithRouteProps } from "../../../../../../state/navigation"
 import { store } from "../../../../../../state/store"
+import { Left, Right } from "../../../../../../data/Either"
+import { This, That, Both } from "../../../../../../data/These"
+import { prettyPrint } from "../../../../../../lib/json"
 
 const detailMapper = (childData: any[]) => ({
   // @ts-ignore
@@ -120,30 +124,23 @@ export function Report(props: WithRouteProps<Props>): JSX.Element {
 
   const [fromStore, dispatch] = useRematch((state) => ({
     configsById: store.select.globalConfig.configsById(state),
+    decodedReportConfigsById: store.select.reports.decodedReportConfigByConfigId(state),
+    decodedQueryConfigsById: store.select.reports.decodedQueryConfigByConfigId(state),
+    reportDataByQuery: state.reports.reportDataByQuery,
   }))
 
-  const reportConfig = record
-    .lookup(reportId, fromStore.configsById)
-    .chain(({ config }) => config)
-    .map((configString) =>
-      tryCatch2v(
-        () => JSON.parse(configString) as ReportConfig, // TODO: Use io-ts
-        (error) => console.log(error)
-      )
-    )
-    .chain((e) => fromEither(e))
+  const reportConfig = record.lookup(reportId, fromStore.decodedReportConfigsById)
 
-  const queryConfig = reportConfig
-    .chain(({ query }) => record.lookup(query.id, fromStore.configsById))
-    .chain(({ config }) => config)
-    .map((configString) =>
-      tryCatch2v(
-        () => JSON.parse(configString) as QueryConfig, // TODO: Use io-ts
-        (error) => console.log(error)
-      )
-    )
-    .chain((e) => fromEither(e))
+  const queryConfig = new Identity(reportConfig)
+    .map((a) => a.chain((b) => b.fold(Left((errs) => none), Right((rc) => rc.query))))
+    .map((a) => a.chain((b) => record.lookup(b.id, fromStore.decodedQueryConfigsById)))
+    .fold(identity)
 
+  console.log("decodedQueryConfigsById >>>", fromStore.decodedQueryConfigsById)
+  reportConfig.foldL(
+    () => console.log("no report config"),
+    (rc) => console.log("report config", rc)
+  )
   const data: any[] = [] // <Query query={query} parameterValues={parameterValues}>
   // {({data, loading, error, refetch}) => {
   //
@@ -160,43 +157,87 @@ export function Report(props: WithRouteProps<Props>): JSX.Element {
 
   return (
     <div>
-      <Typography.Title>{props.title}</Typography.Title>
+      <Typography.Title level={2}>{props.title}</Typography.Title>
+      {these.fromOptions(reportConfig, queryConfig).foldL(
+        None(() => (
+          <Typography.Text type="danger">
+            {`No configuration found for ${reportId}`}
+          </Typography.Text>
+        )),
 
-      {Do(option)
-        .bind("report", reportConfig)
-        .bind("query", queryConfig)
-        .return(identity)
-        .foldL(
-          None(() => <Typography.Text type="danger">No Report Config Found</Typography.Text>),
-          Some(({ report, query }) => {
-            // query.query
-            return (
-              <>
-                {/* <QueryForm parameters={query.parameters} /> */}
-                <Button onClick={() => null}>Fire ze missiles</Button>
-                <GridComponent
-                  ref={grid}
-                  {...commonGridOptions}
-                  toolbarClick={handleToolbarItemClicked(grid)}
-                  // childGrid={{ ...commonGridOptions, ...childGridOptions }}
-                  dataSource={data}
-                  {...report.layout.componentProps}>
-                  <Inject
-                    services={[
-                      Toolbar,
-                      ColumnChooser,
-                      Resize,
-                      DetailRow,
-                      ExcelExport,
-                      PdfExport,
-                      Sort,
-                    ]}
-                  />
-                </GridComponent>
-              </>
+        Some((theseEithers) => {
+          return theseEithers.fold(
+            This((reportConfig1) => (
+              <Typography.Text type="danger">{`No query found for ${reportId}`}</Typography.Text>
+            )),
+
+            That((queryConfig1) => (
+              <Typography.Text type="danger">
+                {`No configuration found for Report config with id ${reportId}`}
+              </Typography.Text>
+            )),
+
+            Both((reportConfig1, queryConfig1) =>
+              these
+                .fromOptions(optionFromEither(reportConfig1), optionFromEither(queryConfig1))
+                .foldL(
+                  None(() => (
+                    <>
+                      <Typography.Paragraph type="danger">
+                        {`Unable to parse Report config with id ${reportId}}`}
+                      </Typography.Paragraph>
+                      <Typography.Paragraph type="danger">
+                        {`Unable to parse Report.Query config associated with Report config with id ${reportId}`}
+                      </Typography.Paragraph>
+                    </>
+                  )),
+
+                  Some((theseConfigs) =>
+                    theseConfigs.fold(
+                      This((reportConfig3) => (
+                        <Typography.Paragraph type="danger">
+                          {`Unable to parse Report.Query config associated with Report config with id ${reportId}`}
+                        </Typography.Paragraph>
+                      )),
+
+                      That((queryConfig3) => (
+                        <Typography.Paragraph type="danger">
+                          {`Unable to parse Report config with id ${reportId}`}
+                        </Typography.Paragraph>
+                      )),
+
+                      Both((reportConfig3, queryConfig3) => (
+                        <>
+                          {/* <QueryForm parameters={query.parameters} /> */}
+                          <Button onClick={() => null}>Fire ze missiles</Button>
+                          <GridComponent
+                            ref={grid}
+                            {...commonGridOptions}
+                            toolbarClick={handleToolbarItemClicked(grid)}
+                            // childGrid={{ ...commonGridOptions, ...childGridOptions }}
+                            dataSource={data}
+                            {...reportConfig3.layout.componentProps}>
+                            <Inject
+                              services={[
+                                Toolbar,
+                                ColumnChooser,
+                                Resize,
+                                DetailRow,
+                                ExcelExport,
+                                PdfExport,
+                                Sort,
+                              ]}
+                            />
+                          </GridComponent>
+                        </>
+                      ))
+                    )
+                  )
+                )
             )
-          })
-        )}
+          )
+        })
+      )}
     </div>
   )
 }
--- DashboardApp/src/state/reports.ts
+++ DashboardApp/src/state/reports.ts
@@ -1,8 +1,15 @@
+import * as record from "fp-ts/lib/Record"
+import { fromOption as eitherFromOption } from "fp-ts/lib/Either"
 import { Left, Right } from "../data/Either"
-import { JSONArray, JSONRecord } from "../data/JSON"
-import { QueryConfig } from "../data/Report"
+import { JSONArray, JSONRecord, JSONRecordCodec, fromStrToJSONRec } from "../data/JSON"
+import { QueryConfig, ReportConfigCodec, QueryConfigCodec } from "../data/Report"
 import { cheapHash } from "../lib/json"
 import * as Store from "./store.types"
+import { PersistedConfig } from "../data/GlobalConfig.Config"
+import { tuple, identity } from "fp-ts/lib/function"
+import { array } from "fp-ts/lib/Array"
+import { None, Some } from "../data/Option"
+import { JSONFromString } from "io-ts-types"
 
 declare module "./store.types" {
   interface AppModels {
@@ -31,7 +38,15 @@ export interface Effects {
   }): void
 }
 
-export interface Selectors {}
+export interface Selectors {
+  decodedReportConfigByConfigId(
+    state: Store.AppState
+  ): Record<PersistedConfig["id"], ReturnType<typeof ReportConfigCodec.decode>>
+
+  decodedQueryConfigByConfigId(
+    state: Store.AppState
+  ): Record<PersistedConfig["id"], ReturnType<typeof QueryConfigCodec.decode>>
+}
 
 export const reports: Store.AppModel<State, Reducers, Effects, Selectors> = {
   state: {
@@ -74,5 +89,55 @@ export const reports: Store.AppModel<State, Reducers, Effects, Selectors> = {
     },
   }),
 
-  selectors: () => ({}),
+  selectors: (slice, createSelector) => ({
+    decodedReportConfigByConfigId(select) {
+      return createSelector(
+        (state) => select.globalConfig.configsByType(state),
+        (configsByType) => {
+          return record
+            .lookup("Report", configsByType)
+            .map((cs) => cs.map(toKvPairs))
+            .map((kvPairs) => record.fromFoldable(array)(kvPairs, identity))
+            .getOrElse({})
+
+          function toKvPairs(c: PersistedConfig) {
+            return tuple(
+              c.id,
+              c.config.foldL(
+                None(() => ReportConfigCodec.decode(null)),
+                Some((config) =>
+                  JSONFromString.decode(config).chain((c) => ReportConfigCodec.decode(c))
+                )
+              )
+            )
+          }
+        }
+      )
+    },
+
+    decodedQueryConfigByConfigId(select) {
+      return createSelector(
+        (state) => select.globalConfig.configsByType(state),
+        (configsByType) => {
+          return record
+            .lookup("Report.Query", configsByType)
+            .map((cs) => cs.map(toKvPairs))
+            .map((kvPairs) => record.fromFoldable(array)(kvPairs, identity))
+            .getOrElse({})
+
+          function toKvPairs(c: PersistedConfig) {
+            return tuple(
+              c.id,
+              c.config.foldL(
+                None(() => QueryConfigCodec.decode(null)),
+                Some((config) =>
+                  JSONFromString.decode(config).chain((c) => QueryConfigCodec.decode(c))
+                )
+              )
+            )
+          }
+        }
+      )
+    },
+  }),
 }
