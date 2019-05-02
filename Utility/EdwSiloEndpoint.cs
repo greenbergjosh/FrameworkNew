5ea6369f7bf4623b99d4177900ae081a50776cd7
Ed Toro <edtoro@live.com>
2019-05-02 10:48:29 -0400

    Code cleanup to remove warnings.

--- Utility/EdwSiloEndpoint.cs
+++ Utility/EdwSiloEndpoint.cs
@@ -1,11 +1,4 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Threading.Tasks;
-using Utility;
+﻿using System.Threading.Tasks;
 using Utility.DataLayer;
 
 namespace Utility
@@ -18,38 +11,43 @@ namespace Utility
         public EdwSiloEndpoint(string dataLayerType, string connectionString)
         {
             this.connectionString = connectionString;
-            this.dataLayerClient = DataLayerClientFactory.DataStoreInstance(dataLayerType);
+            dataLayerClient = DataLayerClientFactory.DataStoreInstance(dataLayerType);
         }
-        public async Task<bool> Audit()
+        public Task<bool> Audit()
         {
             // TODO: Replace audit logic with select top 1 id (nolock) from EDW event table
             // string res = await SqlWrapper.InsertErrorLog(this.connectionString, 1, "EDWLogAudit", "", "", "").ConfigureAwait(false);
             //string result = res.ToLower();
             //if (result == "success") return true;
             //else return false;
-            return true;  // Default to true for now
+            return Task.FromResult(true);  // Default to true for now
         }
 
         public async Task<LoadBalancedWriter.Result> Write(object w, bool secondaryWrite, int timeoutSeconds)
         {
-            string res = await this.dataLayerClient.InsertEdwPayload(this.connectionString, w.ToString(), timeoutSeconds)
+            var res = await dataLayerClient.InsertEdwPayload(connectionString, w.ToString(), timeoutSeconds)
                 .ConfigureAwait(false);
-            string result = res.ToLower();
-            if (result == "success") return LoadBalancedWriter.Result.Success;
+            var result = res.ToLower();
+            if (result == "success")
+            {
+                return LoadBalancedWriter.Result.Success;
+            }
             else if (result == "walkaway")
+            {
                 return LoadBalancedWriter.Result.Walkaway;
-            else if (result == "removeendpoint") return LoadBalancedWriter.Result.RemoveEndpoint;
-            else return LoadBalancedWriter.Result.Failure;
-        }
-        
-        public override bool Equals(object obj)
-        {
-            return ((EdwSiloEndpoint)obj).connectionString == this.connectionString;
+            }
+            else if (result == "removeendpoint")
+            {
+                return LoadBalancedWriter.Result.RemoveEndpoint;
+            }
+            else
+            {
+                return LoadBalancedWriter.Result.Failure;
+            }
         }
 
-        public override int GetHashCode()
-        {
-            return this.connectionString.GetHashCode();
-        }
+        public override bool Equals(object obj) => ((EdwSiloEndpoint)obj).connectionString == connectionString;
+
+        public override int GetHashCode() => connectionString.GetHashCode();
     }
 }
