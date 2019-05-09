9c15351a4e9d819f91bc8e072c32c7d8f4503de3
Ed Toro <edtoro@live.com>
2019-05-09 11:07:02 -0400

    QueueProcessorLib initial commit so Alberto can merge this branch.

---
+++ QueueProcessorLib/QueueProcessor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Utility;
+
+namespace QueueProcessorLib
+{
+    public sealed class QueueProcessor
+    {
+        private static FrameworkWrapper _fw = null;
+        private readonly List<QueueItemProducer> _producers = new List<QueueItemProducer>();
+
+        private int _discriminatorsInRetryRefreshCycleMilliseconds;
+        private readonly bool _runImmediateQueue;
+        private readonly bool _runScheduledQueue;
+        private readonly bool _runRestartQueue;
+        private readonly bool _runRetryQueue;
+
+        public void Config(FrameworkWrapper fw)
+        {
+            try
+            {
+                _fw = fw;
+                _fw.TraceLogging = fw.StartupConfiguration.GetS("Config/Trace").ParseBool() ?? false;
+
+                _discriminatorsInRetryRefreshCycleMilliseconds = int.Parse(_fw.StartupConfiguration.GetS("Config/DiscriminatorsInRetryRefreshCycleMilliseconds").IfNullOrWhitespace("1000"));
+
+            }
+            catch (Exception ex)
+            {
+                _fw?.Error(nameof(Config), ex.UnwrapForLog());
+                throw;
+            }
+        }
+
+        public void OnStart()
+        {
+
+        }
+        public Task Run(HttpContext context) => context.Response.WriteAsync("Hello");
+    }
+}
