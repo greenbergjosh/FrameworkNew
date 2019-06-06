3b298d4a570ea2b2642cc4b3978bb389b33fbf87
Patrick Eisenmann <peisenmann@onpointglobal.com>
2019-06-06 17:02:42 -0400

    Prettier

--- DashboardApp/src/routes/dashboard/routes/user-interface-test/index.tsx
+++ DashboardApp/src/routes/dashboard/routes/user-interface-test/index.tsx
@@ -1,9 +1,4 @@
-import {
-  Button,
-  Card,
-  Divider,
-  Typography
-  } from "antd"
+import { Button, Card, Divider, Typography } from "antd"
 import React from "react"
 import { Helmet } from "react-helmet"
 import { ComponentDefinition } from "../../../../components/interface-builder/components/BaseInterfaceComponent"
