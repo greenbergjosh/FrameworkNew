d0eb706d091b893f1a84619e35c772f610301b4a
Alberto <qbanbpc@gmail.com>
2019-06-26 15:46:47 -0400

    TGWD logging  improvements and disabled user/pass login

--- TheGreatWallOfDataLib/Routing/Authentication.cs
+++ TheGreatWallOfDataLib/Routing/Authentication.cs
@@ -18,7 +18,11 @@ namespace TheGreatWallOfDataLib.Routing
         public static async Task<IGenericEntity> Login(string payload)
         {
             await _fw.Trace(nameof(Login), payload);
-            return JsonWrapper.ToGenericEntity(new {token = _mockToken, name = "Test User", email = "abc@onpointglobal.com", profileImage = "https://picsum.photos/150" });
+            var pl = JsonWrapper.JsonToGenericEntity(payload);
+
+            if (pl.GetS("google").IsNullOrWhitespace()) throw new FunctionException(106, "Auth not implemented and only Google OAuth is faked");
+
+            return JsonWrapper.ToGenericEntity(new { token = _mockToken, name = pl.GetS("google/name"), email = pl.GetS("google/email"), profileImage = pl.GetS("google/imageUrl") });
         }
 
         public static async Task<IGenericEntity> GetUserDetails(string identity)
