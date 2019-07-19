49e2b10559f43cd85ba518cccb776e22c39cddb9
Patrick Eisenmann <peisenmann@onpointglobal.com>
2019-07-19 13:29:43 -0400

    [ChannelAdmin] Improvements to ingestion report

--- ChannelAdminDashboard/DashboardApp/src/routes/dashboard/routes/reports/routes/import-ingestion/config.tsx
+++ ChannelAdminDashboard/DashboardApp/src/routes/dashboard/routes/reports/routes/import-ingestion/config.tsx
@@ -1,7 +1,8 @@
-import moment from 'moment';
-import { Typography } from "antd"
+import { Icon, Typography } from "antd"
+import { ColumnProps } from "antd/lib/table"
+import moment from "moment"
 import React from "react"
-
+import { IngestionStatus } from "../../../../../../state/import-ingestion-report"
 
 export const tableSettings = {
   bordered: false,
@@ -14,46 +15,62 @@ export const tableSettings = {
 
 export const partnerColumns = [
   {
-    title: 'Partners',
-    dataIndex: 'partner',
-    key: 'partner',
+    title: "Partners",
+    dataIndex: "partner",
+    key: "partner",
   },
-];
+]
 
 export const rawDataColumns = [
   {
-    title: 'File',
-    dataIndex: 'file_name',
-    key: 'file_name',
+    title: "File",
+    dataIndex: "file_name",
+    key: "file_name",
   },
   {
-    title: 'Time',
-    dataIndex: 'ts',
-    render: (ts:string) => moment(ts).format('MM/DD/YY h:mm:ss A'),
-    key: 'ts',
+    title: "Time",
+    dataIndex: "ts",
+    render: (ts: string) => moment(ts).format("MM/DD/YY h:mm:ss A"),
+    key: "ts",
   },
   {
-    title: 'Rows',
-    dataIndex: 'rows_processed',
-    key: 'rows_processed',
+    title: "Rows",
+    dataIndex: "rows_processed",
+    key: "rows_processed",
   },
-];
+]
 
 export const importExportDataColumns = [
   {
-    title: 'Table',
-    dataIndex: 'table_name',
-    key: 'table_name',
+    title: "",
+    dataIndex: "succeeded",
+    key: "succeeded",
+    render: (succeeded: boolean) => (
+      <Icon type={succeeded ? "check-circle" : "eclamation-circle"} />
+    ),
   },
   {
-    title: 'Time',
-    dataIndex: 'ts',
-    render: (ts:string) => moment(ts).format('MM/DD/YY h:mm:ss A'),
-    key: 'ts',
+    title: "Table",
+    dataIndex: "table_name",
+    key: "table_name",
+  },
+  // {
+  //   title: 'Time',
+  //   dataIndex: 'ts',
+  //   render: (ts:string) => moment(ts).format('MM/DD/YY h:mm:ss A'),
+  //   key: 'ts',
+  // },
+  {
+    title: "Time",
+    dataIndex: "runtime",
+    render: (runtime: string) => `${runtime}s`,
+    key: "runtime",
+    align: "right",
   },
   {
-    title: 'Rows',
-    dataIndex: 'rows_processed',
-    key: 'rows_processed',
+    title: "Rows",
+    dataIndex: "rows_processed",
+    key: "rows_processed",
+    align: "right",
   },
-];
+] as ColumnProps<IngestionStatus>[]
