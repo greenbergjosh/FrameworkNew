04f17c97079b880f119c8473c2f517747c4af522
Robert Blaske <rblaske@onpointglobal.com>
2019-07-22 13:44:23 -0400

    [ChannelAdmin][CHN-171] Add refresh interval logic; Move config IDs to config constants; Minor code cleanup;

--- ChannelAdminDashboard/DashboardApp/src/routes/dashboard/routes/reports/routes/import-ingestion/config.tsx
+++ ChannelAdminDashboard/DashboardApp/src/routes/dashboard/routes/reports/routes/import-ingestion/config.tsx
@@ -3,6 +3,10 @@ import { ColumnProps } from "antd/lib/table"
 import moment from "moment"
 import React from "react"
 import { IngestionStatus } from "../../../../../../state/import-ingestion-report"
+import * as record from "fp-ts/lib/Record"
+
+export const PARTNER_QUERY_CONFIG_ID = "a3987b6c-3121-4a25-a781-60ff4c4eef25"
+export const INGESTION_STATUS_QUERY_CONFIG_ID = "2327834e-9c0f-4d58-85f8-a774c1520984"
 
 export const tableSettings = {
   bordered: false,
@@ -13,14 +17,6 @@ export const tableSettings = {
   hasData: true,
   pagination: { pageSize: 50 },
 }
-
-export const partnerColumns = [
-  {
-    title: "Partners",
-    dataIndex: "partner",
-    key: "partner",
-  },
-]
 
 export const rawDataColumns = [
   {
@@ -41,7 +37,7 @@ export const rawDataColumns = [
   },
 ]
 
-export const importExportDataColumns = [
+export const importIngestionColumns = [
   {
     title: "",
     dataIndex: "succeeded",
