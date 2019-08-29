e13e04a73b90a9ab81e831ba8c0930efd2d9f064
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-08-29 10:43:43 -0700

    Initial add of AwsCommandGateway code (bare essentials)

---
+++ AwsCommandGateway/AwsDispatchService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using Utility;
+using Jw = Utility.JsonWrapper;
+
+namespace AwsCommandGatewayLib
+
+{
+    public class AwsDispatchService
+    {
+
+        public FrameworkWrapper Fw;
+
+        public void Config(FrameworkWrapper fw)
+        {
+            Fw = fw;
+        }
+
+        public async Task Run (HttpContext context)
+        {
+            try
+            {
+                await context.Response.Body.WriteAsync(Encoding.UTF8.GetBytes(PL.O(new { foobar = Fw.StartupConfiguration.GetS("Config/TestValue") }).ToString()));
+            }
+            catch (Exception ex)
+            {
+                await Fw.Error(nameof(Run), $@"Caught exception processing request: {ex.Message} : {ex.UnwrapForLog()}");
+            }
+        }
+    }
+}
