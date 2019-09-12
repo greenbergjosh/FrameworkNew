a2130c8ad8c95c634528b39f889a0a27e3e50529
Patrick Eisenmann <peisenmann@onpointglobal.com>
2019-09-12 10:14:23 -0400

    Template Manager TS Project

---
+++ mobile-template/src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import "./App.scss"
+import { TemplateManager } from "./components/TemplateManager"
+
+export const App = () => (
+  <div className="template-app">
+    <TemplateManager />
+  </div>
+)
---
+++ mobile-template/src/components/TemplateManager.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const TemplateManager = () => {
+  return <span>Template Manager</span>
+}
