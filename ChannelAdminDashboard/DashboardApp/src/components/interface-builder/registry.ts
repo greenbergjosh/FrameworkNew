e454767a9907482fa15efe14cc842dacad50dc42
Robert Blaske <rblaske@onpointglobal.com>
2019-09-13 15:48:32 -0400

    [ChannelAdmin] Add RTE framework component;

--- ChannelAdminDashboard/DashboardApp/src/components/interface-builder/registry.ts
+++ ChannelAdminDashboard/DashboardApp/src/components/interface-builder/registry.ts
@@ -1,32 +1,33 @@
 import React from "react"
 import { BaseInterfaceComponent } from "./components/base/BaseInterfaceComponent"
-import { SlotConfigInterfaceComponent } from "./components/custom/slot-config/SlotConfigInterfaceComponent"
 import { CardInterfaceComponent } from "./components/display/card/CardInterfaceComponent"
+import { CheckboxInterfaceComponent } from "./components/form/checkbox/CheckboxInterfaceComponent"
+import { CodeEditorInterfaceComponent } from "./components/special/code-editor/CodeEditorInterfaceComponent"
 import { CollapseInterfaceComponent } from "./components/display/collapse/CollapseInterfaceComponent"
 import { ColumnInterfaceComponent } from "./components/display/column/ColumnInterfaceComponent"
-import { ListInterfaceComponent } from "./components/display/list/ListInterfaceComponent"
-import { SectionedNavigationInterfaceComponent } from "./components/display/sectioned-navigation/SectionedNavigationInterfaceComponent"
-import { TableInterfaceComponent } from "./components/display/table/TableInterfaceComponent"
-import { TabsInterfaceComponent } from "./components/display/tabs/TabsInterfaceComponent"
-import { WizardInterfaceComponent } from "./components/display/wizard/WizardInterfaceComponent"
-import { CheckboxInterfaceComponent } from "./components/form/checkbox/CheckboxInterfaceComponent"
 import { DataMapInterfaceComponent } from "./components/form/data-map/DataMapInterfaceComponent"
-import { DateRangeInterfaceComponent } from "./components/form/date-range/DateRangeInterfaceComponent"
 import { DateInterfaceComponent } from "./components/form/date/DateInterfaceComponent"
+import { DateRangeInterfaceComponent } from "./components/form/date-range/DateRangeInterfaceComponent"
 import { FormInterfaceComponent } from "./components/form/FormInterfaceComponent"
 import { InputInterfaceComponent } from "./components/form/input/InputInterfaceComponent"
-import { PasswordInterfaceComponent } from "./components/form/password/PasswordInterfaceComponent"
-import { TextAreaInterfaceComponent } from "./components/form/textarea/TextAreaInterfaceComponent"
+import { ListInterfaceComponent } from "./components/display/list/ListInterfaceComponent"
 import { NumberInputInterfaceComponent } from "./components/form/number-input/NumberInputInterfaceComponent"
 import { NumberRangeInterfaceComponent } from "./components/form/number-range/NumberRangeInterfaceComponent"
+import { PasswordInterfaceComponent } from "./components/form/password/PasswordInterfaceComponent"
 import { ProgressInterfaceComponent } from "./components/display/progress/ProgressInterfaceComponent"
-import { SelectInterfaceComponent } from "./components/form/select/SelectInterfaceComponent"
-import { ToggleInterfaceComponent } from "./components/form/toggle/ToggleInterfaceComponent"
-import { CodeEditorInterfaceComponent } from "./components/special/code-editor/CodeEditorInterfaceComponent"
 import { QueryInterfaceComponent } from "./components/special/query/QueryInterfaceComponent"
 import { RemoteComponentInterfaceComponent } from "./components/special/remote-component/RemoteComponentInterfaceComponent"
+import { RichTextEditorInterfaceComponent } from "./components/form/rich-text-editor/RichTextEditorInterfaceComponent"
+import { SectionedNavigationInterfaceComponent } from "./components/display/sectioned-navigation/SectionedNavigationInterfaceComponent"
+import { SelectInterfaceComponent } from "./components/form/select/SelectInterfaceComponent"
+import { SlotConfigInterfaceComponent } from "./components/custom/slot-config/SlotConfigInterfaceComponent"
+import { TableInterfaceComponent } from "./components/display/table/TableInterfaceComponent"
+import { TabsInterfaceComponent } from "./components/display/tabs/TabsInterfaceComponent"
+import { TextAreaInterfaceComponent } from "./components/form/textarea/TextAreaInterfaceComponent"
+import { ToggleInterfaceComponent } from "./components/form/toggle/ToggleInterfaceComponent"
 import { UploadInterfaceComponent } from "./components/form/upload/UploadInterfaceComponent"
 import { UserInterfaceInterfaceComponent } from "./components/special/user-interface/UserInterfaceInterfaceComponent"
+import { WizardInterfaceComponent } from "./components/display/wizard/WizardInterfaceComponent"
 
 export interface ComponentRegistry {
   cache: ComponentRegistryCache
@@ -57,14 +58,15 @@ export const registry: ComponentRegistry = {
     progress: ProgressInterfaceComponent,
     query: QueryInterfaceComponent,
     "remote-component": RemoteComponentInterfaceComponent,
+    "rich-text-editor": RichTextEditorInterfaceComponent,
     "sectioned-navigation": SectionedNavigationInterfaceComponent,
     select: SelectInterfaceComponent,
-    tabs: TabsInterfaceComponent,
     table: TableInterfaceComponent,
+    tabs: TabsInterfaceComponent,
     textarea: TextAreaInterfaceComponent,
     toggle: ToggleInterfaceComponent,
-    "user-interface": UserInterfaceInterfaceComponent,
     upload: UploadInterfaceComponent,
+    "user-interface": UserInterfaceInterfaceComponent,
     wizard: WizardInterfaceComponent,
 
     // Custom items really should be added as a third party registration
