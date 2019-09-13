b427893f2a1fa5afb2b0f687f4f6dc86169663c7
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-09-13 10:37:12 -0700

    [RegistrationValidation.cs] initial add

---
+++ Utility.OpgAuth/RegistrationValidation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utility.OpgAuth.Sso;
+
+namespace Utility.OpgAuth
+{
+    public class RegistrationValidation
+    {
+        public static bool EmailIsAtOnpointglobal(UserDetails details)
+        {
+            return details.Email.ToLower().EndsWith("@onpointglobal.com");
+        }
+
+    }
+}
