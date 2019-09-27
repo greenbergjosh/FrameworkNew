05ab73adcc6e0cd5f2121b17f10428f15649dc42
Ed Toro <edtoro@live.com>
2019-09-27 15:10:36 -0400

    ReadPixel and Cache expiration implemented.

--- TrackingDataLib/ReadPixel.cs
+++ TrackingDataLib/ReadPixel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Utility;
 using Utility.GenericEntity;
@@ -8,6 +9,39 @@ namespace TrackingDataLib
 {
     internal class ReadPixel
     {
-        internal static Task Process(IGenericEntity parameters, HttpContext context, FrameworkWrapper fw) => throw new NotImplementedException();
+        internal static async Task Process(IGenericEntity parameters, HttpContext context, FrameworkWrapper fw)
+        {
+            var timestamp = DateTime.UtcNow;
+
+            var rsIds = await Common.GetRsIds(parameters);
+
+            var pixelDurationSeconds = int.Parse(fw.StartupConfiguration.GetS("/readPixelDurationSeconds"));
+
+            context.RequestAborted.Register(async () => await DropEvent(timestamp, rsIds, true, fw));
+
+            await Task.Delay(pixelDurationSeconds * 1000);
+
+            if (!context.RequestAborted.IsCancellationRequested)
+            {
+                _ = Task.Run(async () => await DropEvent(timestamp, rsIds, false, fw));
+
+                await Common.WritePixel(context, fw);
+            }
+        }
+
+        private static Task DropEvent(DateTime startTimestamp, Dictionary<string, object> rsIds, bool aborted, FrameworkWrapper fw)
+        {
+            var timestamp = DateTime.UtcNow;
+
+            var duration = aborted ? (timestamp - startTimestamp).TotalMilliseconds : -1;
+
+            var payload = PL.O(new
+            {
+                et = "ReadPixel",
+                duration
+            });
+
+            return Common.DropEvent(payload, rsIds, timestamp, fw);
+        }
     }
 }
