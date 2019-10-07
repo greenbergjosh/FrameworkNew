7193c662ac4c4524bf884dccb70b655242416fa0
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-10-07 10:39:21 -0700

    [Optizmo.cs] better error messages

--- UnsubLib/UnsubFileProviders/Optizmo.cs
+++ UnsubLib/UnsubFileProviders/Optizmo.cs
@@ -83,7 +83,7 @@ namespace UnsubLib.UnsubFileProviders
 
                     if (res.success)
                     {
-                        var resGE = res.success ? Jw.JsonToGenericEntity(res.body) : null;
+                        var resGE = Jw.JsonToGenericEntity(res.body);
                         
                         if (String.Equals(resGE?.GetS("error"), "You do not have access to MD5 downloads", StringComparison.CurrentCultureIgnoreCase))
                         {
@@ -95,9 +95,9 @@ namespace UnsubLib.UnsubFileProviders
 
                         if (!download.IsNullOrWhitespace()) return download;
 
-                        await _fw.Error(_logMethod, $"Optizmo API get file url call failed: {uri} Response: {resGE?.GetS("")}");
+                        await _fw.Error(_logMethod, $"Optizmo API get file url call failed: {baseUrl} derived from: {uri} Response: {resGE?.GetS("")}");
                     }
-                    else await _fw.Error(_logMethod, $"Optizmo API get file url call failed: {uri}");
+                    else await _fw.Error(_logMethod, $"Optizmo API get file call failed against: {baseUrl} derived from: {uri}, with response body: {res.body}");
                 }
             }
             else
