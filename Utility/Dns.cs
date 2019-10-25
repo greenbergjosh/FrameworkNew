563460828ed4230b7f6a171ef3310c69f3f85e45
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-10-25 10:56:22 -0700

    [Dns.cs] Simple DNS lookup class
    * Currently used for finding the reverse hostname of an IP

---
+++ Utility/Dns.cs
@@ -0,0 +1,32 @@
+﻿using Heijden.DNS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utility
+{
+    public static class Dns
+    {
+        public const string DefaultDnsHost = "4.2.2.1";
+
+        public static string ReverseIp(string Ip)
+        {
+            string[] octets = Ip.Trim().Split('.');
+            Array.Reverse(octets);
+            return String.Join(".", octets);
+        }
+
+        public static string ReverseLookupIp (string ip, string dnsHost = DefaultDnsHost)
+        {
+            Response ReverseLookupResponse = new Resolver(dnsHost).Query(ReverseIp(ip) + ".in-addr.arpa", QType.PTR);
+            return ReverseLookupResponse.RecordsPTR.Length > 0 ? ReverseLookupResponse.RecordsPTR[0].ToString() : "";
+        }
+
+        public static string ReverseLookupIp (string ip, Dictionary<string, string> cache, string dnsHost = DefaultDnsHost)
+        {
+            if (cache.ContainsKey(ip) && ! cache[ip].IsNullOrWhitespace()) return cache[ip];
+            cache[ip] = ReverseLookupIp(ip, dnsHost);
+            return cache[ip];
+        }
+    }
+}
