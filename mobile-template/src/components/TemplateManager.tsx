b3fa652758f398f6d51be903d2966ac8883b0446
Patrick Eisenmann <peisenmann@onpointglobal.com>
2019-11-11 16:35:29 -0500

    Updated and genericized template

--- mobile-template/src/components/TemplateManager.tsx
+++ mobile-template/src/components/TemplateManager.tsx
@@ -6,8 +6,7 @@ import {
   Popover
   } from "antd"
 import classNames from "classnames"
-import { invalid } from "moment"
-import React, { SyntheticEvent } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
 import ReactPlayer from "react-player"
 import validURL from "valid-url"
@@ -20,35 +19,14 @@ interface TemplateManagerProps {
 
 export const TemplateManager = ({ getgotService }: TemplateManagerProps) => {
   const [template, setTemplate] = React.useState(DEFAULT_TEMPLATE)
+  React.useEffect(() => {
+    // @ts-ignore We're going to forcibly set this onto the window
+    window.setTemplate = setTemplate
+  }, [setTemplate])
   const [dataState, setDataState] = React.useState({} as {
     [key: string]: string
   })
-  const [popoverState, setPopoverState] = React.useState({} as { [key: string]: boolean })
   const wrapperRef = React.useRef() as React.RefObject<HTMLDivElement>
-
-  // const onClickPhotoElement = React.useCallback((event: SyntheticEvent<HTMLElement>) => {
-  //   console.log(
-  //     "Click Photo Spot",
-  //     event.currentTarget.getAttribute("data-value-key"),
-  //     event.target
-  //   )
-  //   window.GetGotInterface && window.GetGotInterface.selectPhoto()
-  // }, [])
-
-  const onClickPhotoElement = React.useCallback((e: React.MouseEvent<HTMLElement, MouseEvent>) => {
-    let element = e.target as HTMLElement
-
-    while (element.parentElement && !element.getAttribute("data-value-key")) {
-      element = element.parentElement
-    }
-
-    const dataValueKey = element.getAttribute("data-value-key")
-    const dataValue = dataValueKey && dataState[dataValueKey]
-
-    console.log("Click Photo Spot", { dataValueKey, dataValue, element })
-
-    getgotService && getgotService.selectPhoto(dataValueKey)
-  }, [])
 
   const onClickHTMLPhotoElement = React.useCallback((e: Event) => {
     let element = e.target as HTMLElement
@@ -221,7 +199,7 @@ export const TemplateManager = ({ getgotService }: TemplateManagerProps) => {
         })
       }
     }
-  }, [wrapperRef, dataState])
+  }, [wrapperRef, dataState, template])
 
   React.useEffect(() => {
     if (wrapperRef && wrapperRef.current) {
@@ -265,20 +243,11 @@ export const TemplateManager = ({ getgotService }: TemplateManagerProps) => {
         })
       }
     }
-  }, [wrapperRef, dataState])
+  }, [wrapperRef, dataState, template])
 
   return (
     <div ref={wrapperRef}>
       <div dangerouslySetInnerHTML={{ __html: template }} />
-
-      {/* <Divider />
-      <div className="add-photo-icon" onClick={onClickPhotoElement} data-value-key="feedPhoto">
-        <Icon type="camera" theme="filled" /> <span>Add a separate feed photo</span>
-      </div>
-      <p className="disclaimer">
-        This image will appear in others' photo feeds. If you don't want to provide one, we'll
-        automatically select one from above.
-      </p> */}
     </div>
   )
 }
