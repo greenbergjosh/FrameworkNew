f11f1ffcadce1489dc9ddc6f69554545d6e0144d
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-11-12 14:48:15 -0800

    Remove unused code, add proper logging method name

--- UnsubLib/UnsubFileProviders/UnsubCentral.cs
+++ UnsubLib/UnsubFileProviders/UnsubCentral.cs
@@ -9,44 +9,10 @@ using Jw = Utility.JsonWrapper;
 
 namespace UnsubLib.UnsubFileProviders
 {
-    public class UnsubCentral : IUnsubLocationProvider
-    {
-        private readonly FrameworkWrapper _fw;
-        private string _logMethod = $"{nameof(UnsubFileProviders)}.{nameof(UnsubCentral)}";
-
-        public UnsubCentral(FrameworkWrapper fw)
-        {
-            _fw = fw;
-        }
-
-        public bool CanHandle(IGenericEntity network, Uri uri)
-        {
-            if (uri.ToString().Contains("go.unsubcentral.com"))
-            {
-                var qs = HttpUtility.ParseQueryString(uri.Query);
-
-                return qs["key"] != null && qs["s"] != null;
-            }
-
-            return false;
-        }
-
-        public async Task<string> GetFileUrl(IGenericEntity network, Uri uri)
-        {
-            var qs = HttpUtility.ParseQueryString(uri.Query);
-
-            var res = $"https://api.unsubcentral.com/api/service/keys/{qs["key"]}?s={qs["s"]}&format=hash&zipped=true";
-
-            await _fw.Trace(_logMethod, $"Retrieved Unsub location: {uri} -> {res}");
-
-            return res;
-        }
-    }
-
     public class UnsubCentralV2 : IUnsubLocationProvider
     {
         private readonly FrameworkWrapper _fw;
-        private string _logMethod = $"{nameof(UnsubFileProviders)}.{nameof(UnsubCentral)}";
+        private string _logMethod = $"{nameof(UnsubFileProviders)}.{nameof(UnsubCentralV2)}";
 
         public UnsubCentralV2(FrameworkWrapper fw)
         {
