816588dd8238c16e5aca7349341fdb9e1ca49912
Robert Blaske <rblaske@onpointglobal.com>
2019-11-14 16:02:58 -0500

    Update text styles.

--- util/styles.constants.ts
+++ util/styles.constants.ts
@@ -1,19 +1,30 @@
-import { StyleProp, TextStyle } from "react-native"
+import { StyleProp, TextStyle, ViewStyle } from "react-native"
 
 export default {
   H1: {
     fontSize: 34,
     lineHeight: 41,
-    fontWeight: "100",
+    fontWeight: "300",
   } as StyleProp<TextStyle>,
   H2: {
     fontSize: 24,
     lineHeight: 41,
-    fontWeight: "100",
+    fontWeight: "300",
   } as StyleProp<TextStyle>,
   H3: {
     fontSize: 17,
     lineHeight: 18,
-    fontWeight: "normal"
+    fontWeight: "normal",
+    color: "#707070",
   } as StyleProp<TextStyle>,
+  Link: {
+    fontSize: 14,
+    lineHeight: 22,
+    fontWeight: "normal",
+    color: "#007AFF",
+  } as StyleProp<TextStyle>,
+  Button: {
+    minWidth: 350,
+    foo: "bar"
+  } as StyleProp<ViewStyle>,
 }
