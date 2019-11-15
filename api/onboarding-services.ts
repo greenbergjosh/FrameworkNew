7fc62e53bcb4dcc377bd58d5cef55742ff3b6ac3
Robert Blaske <rblaske@onpointglobal.com>
2019-11-15 16:06:56 -0500

    Prettier.

--- api/onboarding-services.ts
+++ api/onboarding-services.ts
@@ -3,25 +3,20 @@ import { GetGotSuccessResponse, getgotRequest } from "./index"
 export interface SendCodeResponse extends GetGotSuccessResponse {}
 export interface SubmitCodeResponse extends GetGotSuccessResponse {}
 export interface CreateUserResponse extends GetGotSuccessResponse {
-  name: string,
-  email: string,
-  handle: string,
-  imageurl: string,
+  name: string
+  email: string
+  handle: string
+  imageurl: string
   h: string
 }
 
-const sid = '62ffd1da-5bc0-480b-9d47-e63df93ade30';
+const sid = "62ffd1da-5bc0-480b-9d47-e63df93ade30"
 
-export const sendCode = async (
-  contact: string
-) => {
+export const sendCode = async (contact: string) => {
   return await getgotRequest<SendCodeResponse>("sendcode", { u: contact })
 }
 
-export const submitCode = async (
-  contact: string,
-  code: string
-) => {
+export const submitCode = async (contact: string, code: string) => {
   return await getgotRequest<SubmitCodeResponse>("submitcnfmcode", { u: contact, code })
 }
 
@@ -32,11 +27,16 @@ export const createUser = async (
   contact: string,
   code: string
 ) => {
-  return await getgotRequest<CreateUserResponse>("createpass", {
-    n: handle, 
-		p: password,
-		d: device, 
-		u: contact, 
-		c: code
-  }, null, sid)
+  return await getgotRequest<CreateUserResponse>(
+    "createpass",
+    {
+      n: handle,
+      p: password,
+      d: device,
+      u: contact,
+      c: code,
+    },
+    null,
+    sid
+  )
 }
