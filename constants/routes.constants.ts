c9466a8a4ae519f734d889af13585776207e5460
Robert Blaske <rblaske@onpointglobal.com>
2019-11-15 08:12:43 -0500

    Add more route constants.

--- constants/routes.constants.ts
+++ constants/routes.constants.ts
@@ -1,13 +1,30 @@
+export enum Authentication {
+  AuthenticationBanned = "AuthenticationBanned",
+  AuthenticationLogin = "AuthenticationLogin",
+}
+export enum Home {
+  Analytics = "Analytics",
+  BlockedUsers = "BlockedUsers",
+  HomeFeed = "HomeFeed",
+  Messages = "Messages",
+  NewMessage = "NewMessage",
+  Notifications = "Notifications",
+  PrivacyOptions = "PrivacyOptions",
+  Tour = "Tour",
+  ViewThread = "ViewThread",
+}
 export enum OnBoarding {
-  Start = "OnBoardingStart",
   CodeEntry = "OnBoardingCodeEntry",
   ResendCode = "OnBoardingResendCode",
-  SetPassword = "OnBoardingSetPassword",
-  SelectInterests = "OnBoardingSelectInterests",
-  SyncContacts = "OnBoardingSyncContacts",
   SelectInfluencers = "OnBoardingSelectInfluencers",
+  SelectInterests = "OnBoardingSelectInterests",
+  SetPassword = "OnBoardingSetPassword",
+  Start = "OnBoardingStart",
+  SyncContacts = "OnBoardingSyncContacts",
 }
 
 export const routes = {
-  OnBoarding
+  Authentication,
+  Home,
+  OnBoarding,
 }
