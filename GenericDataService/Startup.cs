1e26c792b7a676ec24e8e262754972a0c53687e0
Louis-Pierre <lbeaumont@onpointglobal.com>
2019-11-25 11:21:59 -0500

    Add api routes, plug EDW and MemoryCache.

--- GenericDataService/Startup.cs
+++ GenericDataService/Startup.cs
@@ -35,7 +35,6 @@ namespace GenericDataService
                     .SetIsOriginAllowed(x => { return true; })
                     );
             });
-            services.AddMemoryCache();
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
