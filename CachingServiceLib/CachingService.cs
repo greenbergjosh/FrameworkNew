35af1f5347ec3211b2205487520c928e710123f0
Louis-Pierre <lbeaumont@onpointglobal.com>
2019-11-25 11:19:58 -0500

    CachingServiceLib first commit

---
+++ CachingServiceLib/CachingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Utility;
+
+namespace CachingServiceLib
+{
+    public class CachingService
+    {
+        public FrameworkWrapper Fw;
+
+        public void Config(FrameworkWrapper fw)
+        {
+            Fw = fw;
+        }
+
+        public async Task Run(HttpContext context)
+        {
+            try
+            {
+                return;
+            }
+            catch (Exception ex)
+            {
+                await Fw.Error(nameof(Run), $@"Caught exception processing request: {ex.Message} : {ex.UnwrapForLog()}");
+            }
+        }
+
+    }
+}
