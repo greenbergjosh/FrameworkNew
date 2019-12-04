5cc89a671bb53b82cb5061245195b05eb134b2af
Josh Greenberg <greenbergjosh@hotmail.com>
2019-12-04 09:04:00 -0500

    testing

--- QuickTester/Program.cs
+++ QuickTester/Program.cs
@@ -39,6 +39,18 @@ namespace QuickTester
         // If you're looking for something that was in here before, I moved everything into individual static classes in the project. Sorry for the convenience. - Alberto
         private static async Task Main(string[] _args)
         {
+            string fileType = null;
+            List<string> lines = new List<string>() { "email", "bob@hotmail.com", "tom@hotmail.com", "pdfkg" };
+            if (lines.Count > 2 || lines.Last().Length == 0) lines.RemoveAt(lines.Count - 1);
+            var tlines = lines.Select((line, idx) => new Tuple<string, int>(line, idx));
+            var grps = tlines.GroupBy(line => line.Item1.Contains('@') ? "email" : "md5");
+            if (grps.Count() == 1) { fileType = grps.First().Key;  }
+            else if (grps.Count() == 2) 
+            {
+                if (grps.First().Count() == 1 && grps.First().ToArray()[0].Item2 == 0) var ph = 1; // remove header
+                fileType = grps.First().ToArray()[0].Item1;
+            }
+
             //try
             //{
             //    DataFlowTester.ActionTester().GetAwaiter().GetResult();
