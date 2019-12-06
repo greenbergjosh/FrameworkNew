c1ebb1a0ff394025feee2a30bfd0ccff1760355e
Lev Lvovsky <llvovsky@OnpointGlobal.com>
2019-12-06 09:01:09 -0800

    [UnixWrapper.cs] refactor for generic binary search capability

--- Utility/UnixWrapper.cs
+++ Utility/UnixWrapper.cs
@@ -180,8 +180,20 @@ namespace Utility
             }
         }
 
-        public static async Task<(List<string> found, List<string> notFound)> BinarySearchSortedMd5File(string filePath, List<string> keys)
+
+        public static async Task<bool> BinarySearchSortedFile(string filePath, int baseLineLength, string key)
         {
+            var keys = new List<string>() { key };
+            var result = await BinarySearchSortedFile(filePath, baseLineLength, keys);
+
+            return result.found.Count > 0;
+        }
+
+        public static async Task<(List<string> found, List<string> notFound)> BinarySearchSortedFile(string filePath, int baseLineLength, List<string> keys)
+        {
+            int minLineLength = baseLineLength + 1;
+            int maxLineLength = baseLineLength + 2;
+
             var notFound = new List<string>();
             var found = new List<string>();
             var unsubFile = new FileInfo(filePath);
@@ -189,14 +201,14 @@ namespace Utility
             using (var fs = unsubFile.OpenRead())
             {
                 var lineLength = 0;
-                var buffer = new byte[34];
+                var buffer = new byte[maxLineLength];
                 var end = fs.Length;
 
-                await fs.ReadAsync(buffer, 0, 34);
+                await fs.ReadAsync(buffer, 0, maxLineLength);
 
-                if (buffer[32] == 10) lineLength = 33;
-                else if (buffer[32] == 13 && buffer[33] == 10) lineLength = 34;
-                else throw new Exception("Unexpected line termination character");
+                if (buffer[baseLineLength] == 10) lineLength = minLineLength;
+                else if (buffer[baseLineLength] == 13 && buffer[minLineLength] == 10) lineLength = maxLineLength;
+                else throw new Exception($"Unexpected line termination character on line with expected length of: {baseLineLength} with data: {Encoding.UTF8.GetString(buffer, 0, buffer.Length)}");
 
                 var lineCount = end / (decimal)lineLength;
 
@@ -204,9 +216,9 @@ namespace Utility
 
                 var fLength = unsubFile.Length;
 
-                buffer = new byte[32];
+                buffer = new byte[baseLineLength];
 
-                foreach (var md5 in keys)
+                foreach (var key in keys)
                 {
                     var numRec = fLength / lineLength;
                     var bottom = 0L;
@@ -219,69 +231,9 @@ namespace Utility
 
                         fs.Seek(lineLength * cur, SeekOrigin.Begin);
 
-                        await fs.ReadAsync(buffer, 0, 32);
+                        await fs.ReadAsync(buffer, 0, baseLineLength);
 
-                        var cmp = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToLower().CompareTo(md5.ToLower());
-
-                        if (cmp < 0) bottom = cur + 1;
-                        else if (cmp > 0) top = cur - 1;
-                        else
-                        {
-                            f = true;
-                            break;
-                        }
-                    }
-
-                    if (f) found.Add(md5);
-                    else notFound.Add(md5);
-                }
-            }
-
-            return (found, notFound);
-        }
-
-        public static async Task<(List<string> found, List<string> notFound)> BinarySearchSortedSha512File(string filePath, List<string> keys)
-        {
-            var notFound = new List<string>();
-            var found = new List<string>();
-            var unsubFile = new FileInfo(filePath);
-
-            using (var fs = unsubFile.OpenRead())
-            {
-                var lineLength = 0;
-                var buffer = new byte[130];
-                var end = fs.Length;
-
-                await fs.ReadAsync(buffer, 0, 130);
-
-                if (buffer[128] == 10) lineLength = 129;
-                else if (buffer[128] == 13 && buffer[129] == 10) lineLength = 130;
-                else throw new Exception("Unexpected line termination character");
-
-                var lineCount = end / (decimal)lineLength;
-
-                if (lineCount != Math.Ceiling(lineCount)) throw new Exception("Inconsistent line length in file");
-
-                var fLength = unsubFile.Length;
-
-                buffer = new byte[128];
-
-                foreach (var sha512 in keys)
-                {
-                    var numRec = fLength / lineLength;
-                    var bottom = 0L;
-                    var top = numRec - 1;
-                    var f = false;
-
-                    while (bottom <= top)
-                    {
-                        var cur = (top + bottom) / 2;
-
-                        fs.Seek(lineLength * cur, SeekOrigin.Begin);
-
-                        await fs.ReadAsync(buffer, 0, 128);
-
-                        var cmp = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToLower().CompareTo(sha512.ToLower());
+                        var cmp = Encoding.UTF8.GetString(buffer, 0, buffer.Length).ToLower().CompareTo(key.ToLower());
 
                         if (cmp < 0) bottom = cur + 1;
                         else if (cmp > 0) top = cur - 1;
@@ -292,239 +244,12 @@ namespace Utility
                         }
                     }
 
-                    if (f) found.Add(sha512);
-                    else notFound.Add(sha512);
+                    if (f) found.Add(key);
+                    else notFound.Add(key);
                 }
             }
 
             return (found, notFound);
-        }
-
-        public static async Task<(List<string> found, List<string> notFound)> BinarySearchSortedMd5FileV2(string filePath, List<string> keys)
-        {
-            var notFound = new List<string>();
-            var found = new List<string>();
-            var unsubFile = new FileInfo(filePath);
-
-            keys.Sort();
-
-            using (var enrtr = keys.GetEnumerator())
-            using (var fs = unsubFile.OpenRead())
-            {
-                enrtr.MoveNext();
-
-                var lineLength = 0;
-                var buffer = new byte[34];
-                var end = fs.Length;
-
-                await fs.ReadAsync(buffer, 0, 34);
-
-                if (buffer[32] == 10) lineLength = 33;
-                else if (buffer[32] == 13 && buffer[33] == 10) lineLength = 34;
-                else throw new Exception("Unexpected line termination character");
-
-                var lineCount = end / (decimal)lineLength;
-
-                if (lineCount != Math.Ceiling(lineCount)) throw new Exception("Inconsistent line length in file");
-
-                end -= lineLength;
-
-                buffer = new byte[32];
-
-                var top = 0L;
-                var offsetThreshold = lineLength * 3;
-
-                bool EOS(long index)
-                {
-                    return index > end;
-                }
-
-                bool? SortedBelow(string line, string key)
-                {
-                    var c = line.CompareTo(key);
-
-                    return c > 0 ? false : c < 0 ? true : null as bool?;
-                }
-
-                async Task<string> GetIndexValue(long index)
-                {
-                    fs.Seek(index, SeekOrigin.Begin);
-
-                    await fs.ReadAsync(buffer, 0, 32);
-
-                    return Encoding.UTF8.GetString(buffer, 0, 32);
-                }
-
-                async Task<(bool? found, long index, bool? descend)> BinaryJumps()
-                {
-                    var descend = true;
-                    var bottom = end;
-
-                    while (!EOS(top))
-                    {
-                        var lines = (bottom - top) / lineLength;
-                        var index = (((int)Math.Floor(lines / 2M)) * lineLength) + top;
-                        var line = await GetIndexValue(index);
-                        var d = SortedBelow(line, enrtr.Current);
-
-                        if (d == null) return (true, index, null);
-
-                        descend = d.Value;
-
-                        // ToDo: See if moving end index improves performance
-                        if (descend)
-                        {
-                            var ni = index;
-
-                            if (bottom - ni <= offsetThreshold) return (null, index, descend);
-
-                            top = ni;
-                        }
-                        else
-                        {
-                            var ni = index - 1;
-
-                            if (ni - top <= offsetThreshold) return (null, index, descend);
-
-                            bottom = ni;
-                        }
-                    }
-
-                    return (false, -1, null);
-                }
-
-                async Task<bool> search(long index, bool descend)
-                {
-                    string line = null;
-
-                    while (!EOS(index) && (line = await GetIndexValue(index)) != null)
-                    {
-                        var d = SortedBelow(line, enrtr.Current);
-
-                        if (d == null) return true;
-
-                        if (d == false)
-                        {
-                            if (descend)
-                            {
-                                top = index;
-
-                                return false;
-                            }
-                            else index -= lineLength;
-                        }
-                        else if (descend) index += lineLength;
-                        else return false;
-                    }
-
-                    return false;
-                }
-
-                while (!EOS(top) && enrtr.Current != null)
-                {
-                    var r = await BinaryJumps();
-
-                    if (r.found == true) found.Add(enrtr.Current);
-                    else if (r.found == false) notFound.Add(enrtr.Current);
-                    else if (r.index > -1)
-                    {
-                        if (await search(r.index, r.descend.Value)) found.Add(enrtr.Current);
-                        else notFound.Add(enrtr.Current);
-                    }
-                    else
-                    {
-                        while (enrtr.Current != null)
-                        {
-                            notFound.Add(enrtr.Current);
-                            enrtr.MoveNext();
-                        }
-                    }
-
-                    enrtr.MoveNext();
-                }
-            }
-
-            return (found, notFound);
-        }
-
-        public static async Task<bool> BinarySearchSortedMd5File(string sourcePath, string file, string key)
-        {
-            var lkey = key.ToLower();
-            var fPath = sourcePath + "\\" + file;
-
-            var fLength = new System.IO.FileInfo(fPath).Length;
-
-            if (fLength < 33) return false;
-
-            using (var fsSource = new FileStream(fPath, FileMode.Open, FileAccess.Read))
-            {
-                var lLength = 0;
-                var bytes = new byte[34];
-                var n = await fsSource.ReadAsync(bytes, 0, 34);
-
-                if (bytes[32] == 10) lLength = 33;
-                else if (bytes[32] == 13 && bytes[33] == 10) lLength = 34;
-                else throw new System.Exception("Unexpected line termination character");
-
-                var numRec = fLength / lLength;
-
-                long bottom = 0;
-                var top = numRec - 1;
-                while (bottom <= top)
-                {
-                    var cur = (top + bottom) / 2;
-                    fsSource.Seek(lLength * cur, SeekOrigin.Begin);
-                    var nextBytes = new byte[32];
-                    var ct = await fsSource.ReadAsync(nextBytes, 0, 32);
-                    var cmp = Encoding.UTF8.GetString(nextBytes, 0, nextBytes.Length)
-                        .ToLower().CompareTo(lkey);
-                    if (cmp < 0) bottom = cur + 1;
-                    else if (cmp > 0) top = cur - 1;
-                    else return true;
-                }
-            }
-
-            return false;
-        }
-
-        public static async Task<bool> BinarySearchSortedSha512File(string sourcePath, string file, string key)
-        {
-            var lkey = key.ToLower();
-            var fPath = sourcePath + "\\" + file;
-
-            var fLength = new System.IO.FileInfo(fPath).Length;
-
-            if (fLength < 129) return false;
-
-            using (var fsSource = new FileStream(fPath, FileMode.Open, FileAccess.Read))
-            {
-                var lLength = 0;
-                var bytes = new byte[130];
-                var n = await fsSource.ReadAsync(bytes, 0, 130);
-
-                if (bytes[128] == 10) lLength = 129;
-                else if (bytes[128] == 13 && bytes[129] == 10) lLength = 130;
-                else throw new System.Exception("Unexpected line termination character");
-
-                var numRec = fLength / lLength;
-
-                long bottom = 0;
-                var top = numRec - 1;
-                while (bottom <= top)
-                {
-                    var cur = (top + bottom) / 2;
-                    fsSource.Seek(lLength * cur, SeekOrigin.Begin);
-                    var nextBytes = new byte[128];
-                    var ct = await fsSource.ReadAsync(nextBytes, 0, 128);
-                    var cmp = Encoding.UTF8.GetString(nextBytes, 0, nextBytes.Length)
-                        .ToLower().CompareTo(lkey);
-                    if (cmp < 0) bottom = cur + 1;
-                    else if (cmp > 0) top = cur - 1;
-                    else return true;
-                }
-            }
-
-            return false;
         }
 
 
