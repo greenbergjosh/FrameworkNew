97ce1b729947053ec42b562b52c787d8c030b519
Ed Toro <edtoro@live.com>
2020-01-23 19:17:20 -0500

    Made function creation more explicit to avoid confusion when reading code.

--- Utility/DynamicDataFlow/DynamicDataflow.cs
+++ Utility/DynamicDataFlow/DynamicDataflow.cs
@@ -39,6 +39,8 @@ namespace Utility.Dataflow
 
         public Task Completed(IEnumerable<int> blockIds) => Task.WhenAll(blockIds.Select(id => _dataflowBlocks[id].Completion));
 
+        public IDataflowBlock this[int id] => _dataflowBlocks[id];
+
         public Task AllCompleted { get; }
 
         public static DynamicDataflow Create(string config) => Create(JsonWrapper.JsonToGenericEntity(config));
@@ -134,14 +136,18 @@ namespace Utility.Dataflow
         {
             var sd = rw.CompileAndCache(new ScriptDescriptor(null, code));
 
-            return input => rw[sd.Key](new { Input = input }, new StateWrapper());
+            return (TInput input) => rw[sd.Key](new { Input = input }, new StateWrapper());
         }
 
         private static Func<TInput, Task<TOutput>> CreateFunction<TInput, TOutput>(string code, RoslynWrapper rw)
         {
             var sd = rw.CompileAndCache(new ScriptDescriptor(null, code));
 
-            return async input => (TOutput)await rw[sd.Key](new { Input = input }, new StateWrapper());
+            return async (TInput input) =>
+            {
+                Console.WriteLine("running rw");
+                return (TOutput)await rw[sd.Key](new { Input = input }, new StateWrapper());
+            };
         }
 
         private static ExecutionDataflowBlockOptions GetExecutionDataflowBlockOptions(IGenericEntity options, IGenericEntity defaultDataflowBlockOptions)
@@ -203,7 +209,7 @@ namespace Utility.Dataflow
         {
             var sd = rw.CompileAndCache(new ScriptDescriptor(null, code));
 
-            return input => (bool)rw[sd.Key](new { Input = input }, new StateWrapper()).GetAwaiter().GetResult();
+            return (TOutput input) => (bool)rw[sd.Key](new { Input = input }, new StateWrapper()).GetAwaiter().GetResult();
         }
 
         private static DataflowLinkOptions GetDataflowLinkOptions(IGenericEntity options, IGenericEntity defaultDataflowLinkOptions)
