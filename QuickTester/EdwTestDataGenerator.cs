a0e239e8efc2f8d028ec0ec4d0d75e0d4a929482
Josh Greenberg <greenbergjosh@hotmail.com>
2020-07-02 14:50:00 -0400

    latest version

--- QuickTester/EdwTestDataGenerator.cs
+++ QuickTester/EdwTestDataGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Utility;
@@ -18,16 +19,18 @@ namespace QuickTester
 
             var startDate = DateTime.Parse("01/01/2020");
 
-            var dates = Enumerable.Range(0, 3).Select(i => startDate.AddDays(i));
+            var dates = Enumerable.Range(0, 10).Select(i => startDate.AddDays(i));  //10
 
-            var hours = Enumerable.Range(13, 4);
+            var hours = Enumerable.Range(1, 10);  //10
 
-            var subCampaigns = Enumerable.Range(1, 10);
+            var subCampaigns = Enumerable.Range(1, 100);  //100
 
-            var ages = Enumerable.Range(30, 5);
+            var ages = Enumerable.Range(1, 100);  //100
 
-            var events = new List<EdwBulkEvent>();
-
+            int i = 0;
+            var folder = GetFolder("OneFile");
+            var file = Path.Combine(folder, "events.txt");
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(file);
             foreach (var date in dates)
             {
                 foreach (var hour in hours)
@@ -38,6 +41,9 @@ namespace QuickTester
                     {
                         foreach (var age in ages)
                         {
+                            //var events = new List<EdwBulkEvent>();
+                            StringBuilder sb = new StringBuilder();
+
                             var eventGuids = Enumerable.Range(1, 10).Select(_ => Guid.NewGuid()).ToArray();
                             var rs1Id = eventGuids[0];
                             var rs2Id = eventGuids[1];
@@ -50,55 +56,60 @@ namespace QuickTester
 
                             var e = new EdwBulkEvent();
                             e.AddRS(EdwBulkEvent.EdwType.Immediate, rs1Id, targetDate, PL.O(new { s = subCampaign }), rs1ConfigId); // file export type = RS1
-                            events.Add(e);
+                            if (i == 0) sb.Append(e.ToString());
+                            else sb.Append("|" + e.ToString());
+                            i++;
 
                             e = new EdwBulkEvent();
                             e.AddRS(EdwBulkEvent.EdwType.Checked, rs2Id, targetDate, PL.O(new {s = subCampaign }), rs2ConfigId); // file export type = RS2 - 1
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[2], targetDate, rss, null, PL.O(new { et = "GPImpression", p1 = 1, p4 = 4, p6 = 6, p8 = 8 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[3], targetDate, rss, null, PL.O(new { et = "PImpression", p2 = 2, p5 = 5, p7 = 7, p8 = 80 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[4], targetDate, rss, new[] { eventGuids[3].ToString(), eventGuids[2].ToString() }, PL.O(new { et = "Impression", p3 = 3, p6 = 60, p7 = 70, p8 = 800 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[5], targetDate, rss, null, PL.O(new { et = "GPClick", q1 = 1, q4 = 4, q5 = 5, q7 = 7 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[6], targetDate, rss, null, PL.O(new { et = "PClick", q2 = 2, q4 = 40, q6 = 6, q7 = 70 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEventWhepD(eventGuids[7], targetDate, rss, new Dictionary<string, object>() { { "bob", eventGuids[5] }, { "tom", eventGuids[6] } }, PL.O(new { et = "Click", q3 = 3, q5 = 50, q6 = 60, q7 = 700 }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[8], targetDate, rss, null, PL.O(new { et = "Survey", age }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddRS(EdwBulkEvent.EdwType.CheckedDetail, rs2Id, targetDate, PL.O(new { age }), rs2ConfigId); // file export type = RS2 - 1
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
 
                             e = new EdwBulkEvent();
                             e.AddEvent(eventGuids[9], targetDate, rss, null, PL.O(new { et = "Action" }));
-                            events.Add(e);
+                            sb.Append("|" + e.ToString());
+
+                            sw.Write(sb.ToString());
                         }
                     }
                 }
             }
 
-            OneFile(events);
-            FilePerEventType(events);
-            RandomFiles(events);
+            sw.Close();
+            //OneFile(events);
+            //FilePerEventType(events);
+            //RandomFiles(events);
         }
 
         private static void OneFile(IEnumerable<EdwBulkEvent> events)
