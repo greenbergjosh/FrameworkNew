fa31f1d59126e43e3bf53c110a0d014b1b8539d8
Robert Blaske <rblaske@onpointglobal.com>
2020-09-16 08:38:24 -0400

    [DashboardApp][CHN-359] Remove QueryBuilder component wrapper since it is not needed. Upgrade syncfusion grid. Move SlotConfigInterfaceComponent from "Custom" tab to "Special" tab.

--- DashboardApp/src/components/custom-ib-components/query-builder/types.ts
+++
@@ -1,9 +0,0 @@
-import { QueryBuilder } from "@opg/interface-builder"
-import { PersistedConfig } from "../../../data/GlobalConfig.Config"
-
-export interface QueryBuilderInterfaceComponentProps extends QueryBuilder.QueryBuilderInterfaceComponentProps {
-  schemaRawConfigId: PersistedConfig["id"]
-}
-export interface QueryBuilderInterfaceComponentState extends QueryBuilder.QueryBuilderInterfaceComponentState {
-  schemaRaw: string
-}
